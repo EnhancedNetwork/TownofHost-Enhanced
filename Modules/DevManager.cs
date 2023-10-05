b0b871d9039958af6ac182a0288fa92b03ab5176
ryuk2098 <nehal.birla98@gmail.com>
2023-10-06 03:26:37 +0530

    Renamed roles and added patreon info 1. Renamed Assassin back to Evil Guesser 2. Renamed Vigilante back to Nice Guesser 3. Renamed Jailor to Jailer (for better english) 4. added mising patren info

--- Modules/DevManager.cs
+++ Modules/DevManager.cs
@@ -179,6 +179,7 @@ public static class DevManager
         DevUserList.Add(new(code: "cannylink#0564", color: "null", tag: "null", isUp: true, isDev: false, deBug: false, colorCmd: false, upName: "SpicyPoops"));
         DevUserList.Add(new(code: "examfishy#9080", color: "null", tag: "null", isUp: true, isDev: false, deBug: false, colorCmd: false, upName: "killer5362"));
         DevUserList.Add(new(code: "dusksole#6956", color: "null", tag: "null", isUp: true, isDev: false, deBug: false, colorCmd: false, upName: "Bandz"));
+        DevUserList.Add(new(code: "wontsave#5153", color: "null", tag: "null", isUp: true, isDev: false, deBug: false, colorCmd: false, upName: "wayne"));
         DevUserList.Add(new(code: "rollingegg#7687", color: "#fe7d6e", tag: "Ruler of Jiggly Peach Cakes", isUp: true, isDev: false, deBug: false, colorCmd: false, upName: "DarlingXX"));
     }
     public static bool IsDevUser(this string code) => DevUserList.Any(x => x.Code == code);
