e07bb8da77af788a4c1e3b2582b38d6803acc19d
ryuk2098 <nehal.birla98@gmail.com>
2023-10-09 10:01:41 +0530

    fix increase and decrease condition for reverie

--- Roles/Crewmate/Reverie.cs
+++ Roles/Crewmate/Reverie.cs
@@ -55,7 +55,7 @@ public static class Reverie
         if (killer == null || target == null) return;
         if (!IsEnable || !killer.Is(CustomRoles.Reverie)) return;
         float kcd;
-        if (target.GetCustomRole().IsCrewmate()) kcd = NowCooldown[killer.PlayerId] - ReduceKillCooldown.GetFloat();
+        if (!target.GetCustomRole().IsCrewmate()) kcd = NowCooldown[killer.PlayerId] - ReduceKillCooldown.GetFloat();
         else kcd = NowCooldown[killer.PlayerId] + IncreaseKillCooldown.GetFloat();
         NowCooldown[killer.PlayerId] = Math.Clamp(kcd, MinKillCooldown.GetFloat(), MaxKillCooldown.GetFloat());
         killer.ResetKillCooldown();
