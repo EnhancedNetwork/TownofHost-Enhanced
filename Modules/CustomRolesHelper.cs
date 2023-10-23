2cfbc1dc6ab7b0e06196121adba656bbda509440
ryuk2098 <nehal.birla98@gmail.com>
2023-10-23 23:20:46 +0530

    lurker incompatible with circumvent and removed sponsor

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -1407,7 +1407,8 @@ static class CustomRolesHelper
                     || pc.Is(CustomRoles.Witch) && Witch.ModeSwitchAction.GetValue() == 1
                     || pc.Is(CustomRoles.Swooper)
                     || pc.Is(CustomRoles.Wildling)
-                    || pc.Is(CustomRoles.Minimalism))
+                    || pc.Is(CustomRoles.Minimalism)
+                    || pc.Is(CustomRoles.Lurker))
                     return false;
                 if (!pc.GetCustomRole().IsImpostor())
                     return false;
