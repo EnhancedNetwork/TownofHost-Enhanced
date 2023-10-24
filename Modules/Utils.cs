b2336c9ef1a5972743734055343d6a47fbab7832
Tommy XL <fadeevvladimir16@gmail.com>
2023-10-24 23:43:32 +0800

    Fix code

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1729,7 +1729,7 @@ public static class Utils
             {
                 if (!player.IsModClient()) return;
                 {
-                    if ((GameStates.IsOnlineGame || GameStates.IsLocalGame) && Main.AutoMuteUs.Value != true)
+                    if ((GameStates.IsOnlineGame || GameStates.IsLocalGame) && !Main.AutoMuteUs.Value)
                         name = $"<color={GetString("HostColor")}>{GetString("HostText")}</color><color={GetString("IconColor")}>{GetString("Icon")}</color><color={GetString("NameColor")}>{name}</color>";
 
                     //name = $"<color=#902efd>{GetString("HostText")}</color><color=#4bf4ff>♥</color>" + name;
