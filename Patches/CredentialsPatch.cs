742908067c9a4d23825c73409f363cd1663d24a3
Dxgamer7405 <ponteseduardo391@gmail.com>
2023-10-29 01:04:27 -0400

    ta essa é a ultima

--- Patches/CredentialsPatch.cs
+++ Patches/CredentialsPatch.cs
@@ -72,7 +72,7 @@ public static class Credentials
             Main.credentialsText = $"\r\n<color={Main.ModColor}>{Main.ModName}</color> v{Main.PluginDisplayVersion}";
 
 #if RELEASE
-            Main.credentialsText += $"\r\npor <color=#000000>Equipe Y9";
+            Main.credentialsText += $"\r\npor <color=#32cd32>Equipe Y9";
 #endif
 
 #if CANARY
