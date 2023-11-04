f4c260fc1d2decdb24b095e08b50011d20e5addc
ryuk2098 <nehal.birla98@gmail.com>
2023-11-04 18:03:22 +0530

    remove using

--- main.cs
+++ main.cs
@@ -4,7 +4,6 @@ using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Il2CppInterop.Runtime.Injection;
-using Il2CppSystem.Security.Cryptography;
 using System;
 using System.Collections.Generic;
 using System.IO;
