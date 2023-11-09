4a3a5b169f1f10fe6043b1f3cdf45b9b2cb54133
PHGaming121 <76562427+PHGaming121@users.noreply.github.com>
2023-11-09 12:07:19 -0300

    como ficar gigante ;-;

---
+++ Mods/Funny/SizePatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+
+namespace TOHE
+{
+    [HarmonyPatch]
+    public static class SizePatch
+    {
+        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+        [HarmonyPostfix]
+        public static void Postfix(HudManager __instance)
+        {
+            PlayerControl playerControl = PlayerControl.LocalPlayer.GetComponent<PlayerControl>();
+            Vector3 playerLocalScale = PlayerControl.LocalPlayer.transform.localScale;
+            if (Main.BigSize.Value == true)
+            {
+                //Set sprite scale
+            PlayerControl.LocalPlayer.transform.localScale = new Vector3 (5f, 5f, 1f);
+            }
+            else
+            {
+                //Set sprite scale
+                PlayerControl.LocalPlayer.transform.localScale = new Vector3 (0.7f, 0.7f, 1f);
+            }
+        }
+    }
+}
