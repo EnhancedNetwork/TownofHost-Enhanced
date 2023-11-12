3f6d8ae6c90328d307610bd1f4d84ca69881ae10
PHGaming121 <76562427+PHGaming121@users.noreply.github.com>
2023-11-12 14:45:32 -0300

    Revert "Delete Mods directory"
    This reverts commit 177753ead89593ad33d2356eb6bd3366ff6c52a4.

---
+++ Mods/Passive/PenaltyPatch.cs
@@ -0,0 +1,20 @@
+
+using HarmonyLib;
+
+namespace TOHE;
+
+[HarmonyPatch(typeof(StatsManager), nameof(StatsManager.BanMinutesLeft), MethodType.Getter)]
+public static class Passive_BanGetterPrefix
+{
+    //Prefix patch of Getter method for StatsManager.BanMinutesLeft to remove disconnect penalty
+    public static bool Prefix(StatsManager __instance, ref int __result)
+    {
+        if (!Main.AvoidBans.Value == true){
+            return true; //Only works if CheatSettings.avoidBans is enabled
+        }
+        
+        __instance.BanPoints = 0f; //Removes all BanPoints
+        __result = 0; //Removes all BanMinutes
+        return false;
+    }
+}
---
+++ Mods/Tasks/FakeTasks.cs
@@ -0,0 +1,34 @@
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace TOHE;
+
+[HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.Start))]
+public static class Console_UsePrefix
+{
+    public static void Prefix(ShipStatus __instance)
+    {
+        Console[] consoles = UnityEngine.Object.FindObjectsOfType<Console>();
+
+        if(Main.ImpTasks.Value == true)
+        { 
+        foreach (var console in consoles)
+        {
+            // Set AllowImpostor to true for each Console instance
+            console.AllowImpostor = true;
+        }
+        
+        }
+        {
+            foreach (var console in consoles)
+            {
+                // Set AllowImpostor to true for each Console instance
+                return;
+            }
+
+        }
+
+    }
+
+}
