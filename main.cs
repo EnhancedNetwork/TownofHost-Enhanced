4fa02b3df13545436e20f3d2b7cdb61ddf6d57dc
ryuk2098 <nehal.birla98@gmail.com>
2023-11-27 04:43:29 +0530

    change ver

--- main.cs
+++ main.cs
@@ -40,12 +40,12 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "1.2.1.6";
-    public const string PluginDisplayVersion = "1.2.1 Canary 2";
+    public const string PluginVersion = "1.2.1";
+    public const string PluginDisplayVersion = "1.2.1";
     public static readonly string SupportedVersionAU = "2023.10.24";
     /******************* Change one of the three variables to true before making a release. *******************/
-    public const bool Canary = true; // Unused variable? ---- not unused anymore :)
-    public const bool fullRelease = false;
+    public const bool Canary = false; // Unused variable? ---- not unused anymore :)
+    public const bool fullRelease = true;
     public const bool devRelease = false;
 
     public static bool hasAccess = true;
