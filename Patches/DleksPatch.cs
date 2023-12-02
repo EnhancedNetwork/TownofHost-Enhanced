e3862fe6d53d8df430571abc4ed36262fb01da2e
Tommy XL <fadeevvladimir16@gmail.com>
2023-12-03 04:09:14 +0800

    Add File "DleksPatch" & "HorseModePatch"

---
+++ Patches/DleksPatch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TOHE.Patches;
+
+// Thanks Galster (https://github.com/Galster-dev)
+[HarmonyPatch(typeof(AmongUsClient._CoStartGameHost_d__30), nameof(AmongUsClient._CoStartGameHost_d__30.MoveNext))]
+class DleksPatch
+{
+    private static bool Prefix(AmongUsClient._CoStartGameHost_d__30 __instance, ref bool __result)
+    {
+        if (__instance.__1__state != 0)
+        {
+            return true;
+        }
+
+        __instance.__1__state = -1;
+        if (LobbyBehaviour.Instance)
+        {
+            LobbyBehaviour.Instance.Despawn();
+        }
+
+        if (ShipStatus.Instance)
+        {
+            __instance.__2__current = null;
+            __instance.__1__state = 2;
+            __result = true;
+            return false;
+        }
+
+        // removed dleks check as it's always false
+        var num2 = Mathf.Clamp(GameOptionsManager.Instance.CurrentGameOptions.MapId, 0, Constants.MapNames.Length - 1);
+        __instance.__2__current = __instance.__4__this.ShipLoadingAsyncHandle = __instance.__4__this.ShipPrefabs[num2].InstantiateAsync();
+        __instance.__1__state = 1;
+
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(KeyValueOption), nameof(KeyValueOption.OnEnable))]
+class AutoselectDleksPatch
+{
+    private static void Postfix(KeyValueOption __instance)
+    {
+        if (__instance.Title == StringNames.GameMapName)
+        {
+            // vanilla clamps this to not autoselect dleks
+            __instance.Selected = GameOptionsManager.Instance.CurrentGameOptions.MapId;
+        }
+    }
+}
--- Patches/MainMenuManagerPatch.cs
+++ Patches/MainMenuManagerPatch.cs
@@ -291,68 +291,11 @@ public static class MainMenuManagerPatch
     [HarmonyPostfix]
     public static void OpenMenuPostfix()
     {
-        if (Credentials.ToheLogo != null) Credentials.ToheLogo.gameObject.SetActive(false);
+        if (ToheLogo != null) ToheLogo.gameObject.SetActive(false);
     }
     [HarmonyPatch(nameof(MainMenuManager.ResetScreen)), HarmonyPostfix]
     public static void ResetScreenPostfix()
     {
-        if (Credentials.ToheLogo != null) Credentials.ToheLogo.gameObject.SetActive(true);
-    }
-}
-
-// 来源：https://github.com/ykundesu/SuperNewRoles/blob/master/SuperNewRoles/Patches/HorseModePatch.cs
-[HarmonyPatch(typeof(Constants), nameof(Constants.ShouldHorseAround))]
-public static class HorseModePatch
-{
-    public static bool Prefix(ref bool __result)
-    {
-        __result = Main.HorseMode.Value;
-        return false;
-    }
-}
-[HarmonyPatch(typeof(AmongUsClient._CoStartGameHost_d__30), nameof(AmongUsClient._CoStartGameHost_d__30.MoveNext))]
-public static class DleksPatch
-{
-    public static bool Prefix(AmongUsClient._CoStartGameHost_d__30 __instance, ref bool __result)
-    {
-        if (__instance.__1__state != 0)
-        {
-            return true;
-        }
-            
-        __instance.__1__state = -1;
-        if (LobbyBehaviour.Instance)
-        {
-            LobbyBehaviour.Instance.Despawn();
-        }
-
-        if (ShipStatus.Instance)
-        {
-            __instance.__2__current = null;
-            __instance.__1__state = 2;
-            __result = true;
-            return false;
-        }
-        
-        // removed dleks check as it's always false
-        var num2 = Mathf.Clamp(GameOptionsManager.Instance.CurrentGameOptions.MapId, 0, Constants.MapNames.Length - 1);
-        __instance.__2__current = __instance.__4__this.ShipLoadingAsyncHandle = __instance.__4__this.ShipPrefabs[num2].InstantiateAsync();
-        __instance.__1__state = 1;
-
-        __result = true;
-        return false;
-    }
-}
-
-[HarmonyPatch(typeof(KeyValueOption), nameof(KeyValueOption.OnEnable))]
-public static class AutoselectDleksPatch
-{
-    public static void Postfix(KeyValueOption __instance)
-    {
-        if (__instance.Title == StringNames.GameMapName)
-        {
-            // vanilla clamps this to not autoselect dleks
-            __instance.Selected = GameOptionsManager.Instance.CurrentGameOptions.MapId;
-        }
+        if (ToheLogo != null) ToheLogo.gameObject.SetActive(true);
     }
 }
