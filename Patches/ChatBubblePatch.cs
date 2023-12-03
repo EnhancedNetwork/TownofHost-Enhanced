fa54c5f48dd647269f9796015f2ec025eff77ae8
PHGaming121 <76562427+PHGaming121@users.noreply.github.com>
2023-12-03 13:10:24 -0300

    test

--- Patches/ChatBubblePatch.cs
+++ Patches/ChatBubblePatch.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using UnityEngine;
 
 namespace TOHE.Patches;
 
@@ -10,12 +11,24 @@ class ChatBubbleSetRightPatch
         if (Main.isChatCommand) __instance.SetLeft();
     }
 }
-[HarmonyPatch(typeof(ChatBubble), nameof(ChatBubble.SetName))]
-class ChatBubbleSetNamePatch
+[HarmonyPatch(typeof(ChatBubble))]
+public static class ChatBubblePatch
 {
-    public static void Postfix(ChatBubble __instance)
+    [HarmonyPatch(nameof(ChatBubble.SetName)), HarmonyPostfix]
+    public static void SetName_Postfix(ChatBubble __instance)
     {
         if (GameStates.IsInGame && __instance.playerInfo.PlayerId == PlayerControl.LocalPlayer.PlayerId)
             __instance.NameText.color = PlayerControl.LocalPlayer.GetRoleColor();
+    }
+    [HarmonyPatch(nameof(ChatBubble.SetText)), HarmonyPrefix]
+    public static void SetText_Prefix(ChatBubble __instance, ref string chatText)
+    {
+        bool modded = PlayerControl.LocalPlayer.IsModClient();
+        __instance.Background.color = modded ? Color.black : Color.white;
+
+        if (modded)
+        {
+            chatText = Utils.ColorString(Color.white, chatText.TrimEnd('\0'));
+        }
     }
 }
---
+++ Patches/TextBoxPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace TOHE;
+
+[HarmonyPatch(typeof(TextBoxTMP))]
+public class TextBoxPatch
+{
+    static Dictionary<string, string> replaceDic = new()
+            {
+                { "（", " (" },
+                { "）", ") " },
+                { "，", ", " },
+                { "：", ": " },
+                { "[", "【" },
+                { "]", "】" },
+                { "‘", " '" },
+                { "’", "' " },
+                { "“", " ''" },
+                { "”", "'' " },
+                { "！", "! " },
+            };
+    [HarmonyPatch(nameof(TextBoxTMP.SetText)), HarmonyPrefix]
+    public static bool ModifyCharacterLimit(TextBoxTMP __instance, [HarmonyArgument(0)] string input, [HarmonyArgument(1)] string inputCompo = "")
+    {
+        __instance.characterLimit = AmongUsClient.Instance.AmHost ? 1999 : 400;
+        if (input.Length < 1) return true;
+        string before = input[^1..];
+        if (replaceDic.TryGetValue(before, out var after))
+        {
+            __instance.SetText(input.Replace(before, after));
+            return false;
+        }
+        return true;
+    }
+}
