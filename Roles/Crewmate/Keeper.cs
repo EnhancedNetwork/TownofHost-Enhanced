4db3c792eabd80ebcd38f407eb3d9ffd0911a226
ryuk2098 <nehal.birla98@gmail.com>
2023-12-13 02:26:55 +0530

    keeper half done

---
+++ Roles/Crewmate/Keeper.cs
@@ -0,0 +1,124 @@
+﻿using Hazel;
+using System.Collections.Generic;
+using UnityEngine;
+using static TOHE.Options;
+using static TOHE.Translator;
+
+namespace TOHE.Roles.Crewmate;
+public static class Keeper
+{
+    private static readonly int Id = 6600;
+    //public static List<byte> playerIdList = new();
+    public static bool IsEnable = false;
+
+    public static List<byte> keeperTarget = new();
+    public static Dictionary<byte, int> keeperUses = new();
+    public static Dictionary<byte, bool> DidVote = new();
+
+    public static OptionItem KeeperUsesOpt;
+    public static OptionItem HideVote;
+    public static OptionItem AbilityUseGainWithEachTaskCompleted;
+
+    public static void SetupCustomOption()
+    {
+        SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Cleanser);
+        KeeperUsesOpt = IntegerOptionItem.Create(Id + 10, "MaxProtections", new(1, 14, 1), 3, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Keeper])
+            .SetValueFormat(OptionFormat.Times);
+        HideVote = BooleanOptionItem.Create(Id + 11, "KeeperHideVote", false, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Keeper]);
+        //    AbilityUseGainWithEachTaskCompleted = IntegerOptionItem.Create(Id + 12, "AbilityUseGainWithEachTaskCompleted", new(0, 5, 1), 1, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Cleanser])
+        //        .SetValueFormat(OptionFormat.Times);
+
+    }
+    public static void Init()
+    {
+        //playerIdList = new();
+        keeperTarget = new();
+        keeperUses = new();
+        DidVote = new();
+        IsEnable = false;
+    }
+
+    public static void Add(byte playerId)
+    {
+        //playerIdList.Add(playerId);
+        DidVote.Add(playerId, false);
+        IsEnable = true;
+    }
+
+    //public static string GetProgressText(byte playerId) => Utils.ColorString(CleanserUsesOpt.GetInt() - CleanserUses[playerId] > 0 ? Utils.GetRoleColor(CustomRoles.Cleanser).ShadeColor(0.25f) : Color.gray, CleanserUses.TryGetValue(playerId, out var x) ? $"({CleanserUsesOpt.GetInt() - x})" : "Invalid");
+    public static string GetProgressText(byte playerId)
+    {
+        if (!keeperUses.ContainsKey(playerId)) return "Invalid";
+        Color x;
+        if (KeeperUsesOpt.GetInt() - keeperUses[playerId] > 0)
+            x = Utils.GetRoleColor(CustomRoles.Cleanser);
+        else x = Color.gray;
+        return (Utils.ColorString(x, $"({KeeperUsesOpt.GetInt() - keeperUses[playerId]})"));
+    }
+
+    private static void SendRPC(int type, byte keeperId = 0xff, byte targetId = 0xff)
+    {
+        MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.KeeperRPC, SendOption.Reliable, -1);
+        writer.Write(type);
+        if (type == 0)
+        {
+            writer.Write(keeperId);
+            writer.Write(keeperUses[keeperId]);
+            writer.Write(targetId);
+        }
+        AmongUsClient.Instance.FinishRpcImmediately(writer);
+    }
+
+    public static void ReceiveRPC(MessageReader reader)
+    {
+        int type = reader.ReadInt32();
+        if (type == 0)
+        {
+            byte keeperId = reader.ReadByte();
+            DidVote[keeperId] = true;
+            
+            int uses = reader.ReadInt32();
+            keeperUses[keeperId] = uses;
+
+            byte targetId = reader.ReadByte();
+            if (!keeperTarget.Contains(targetId)) keeperTarget.Add(targetId);
+        }
+
+        else if (type == 1)
+        {
+            foreach (var pid in DidVote.Keys)
+            {
+                DidVote[pid] = false;
+            }
+            keeperTarget.Clear();
+        }
+    }
+
+    public static void OnVote(PlayerControl voter, PlayerControl target)
+    {
+        if (!voter.Is(CustomRoles.Keeper)) return;
+        if (DidVote[voter.PlayerId]) return;
+        DidVote[voter.PlayerId] = true;
+        if (keeperTarget.Contains(target.PlayerId)) return;
+        if (keeperUses[voter.PlayerId] >= KeeperUsesOpt.GetInt()) return;
+
+        keeperUses[voter.PlayerId]++;
+        keeperTarget.Add(target.PlayerId);
+        Logger.Info($"{voter.GetNameWithRole()} chosen as keeper target by {target.GetNameWithRole()}", "Keeper");
+        SendRPC(type:0, keeperId: voter.PlayerId, targetId: target.PlayerId); // add keeperUses, KeeperTarget and DidVote
+    }
+
+    public static void AfterMeetingTasks()
+    {
+        if (!IsEnable) return;
+
+        foreach (var pid in DidVote.Keys)
+        {
+            DidVote[pid] = false;
+        }
+        keeperTarget.Clear();
+        SendRPC(type: 1);
+    }
+
+    public static bool BeforeExile(byte exileId) => keeperTarget.Contains(exileId);
+}
