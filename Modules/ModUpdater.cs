6a6a117269fcbe2ade7e94c1a970cc03f01f5afa
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2023-12-13 18:57:41 +0800

    更新 ModUpdater.cs

--- Modules/ModUpdater.cs
+++ Modules/ModUpdater.cs
@@ -178,6 +178,7 @@ public class ModUpdater
             {
                 if (path.EndsWith(Path.GetFileName(Assembly.GetExecutingAssembly().Location))) continue;
                 if (path.EndsWith("TOHE.dll")) continue;
+                if (!path.EndsWith(".dll")) continue;
                 Logger.Info($"{Path.GetFileName(path)} 已删除", "DeleteOldFiles");
                 File.Delete(path);
             }
