384aa3b1b3ff7c9d4568626c6f30b5791832d082
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2023-12-19 12:48:39 +0800

    Update eac ban list

---
+++ Patches/VoteBanSystemPatch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+namespace TOHE.Patches;
+
+[HarmonyPatch(typeof(VoteBanSystem), nameof(VoteBanSystem.AddVote))]
+internal class VoteBanSystemPatch
+{
+    public static bool Prefix(VoteBanSystem __instance, [HarmonyArgument(0)] int srcClient, [HarmonyArgument(1)] int clientId)
+    {
+        if (!AmongUsClient.Instance.AmHost) return true;
+
+        if (DebugModeManager.AmDebugger)
+            return false; //Disable vote kick while debugging
+
+        var VoterClient = AmongUsClient.Instance.GetClient(srcClient);
+        var ClientData = AmongUsClient.Instance.GetClient(clientId);
+        var VoterPc = VoterClient.Character;
+        var ClientPc = ClientData.Character;
+
+        if (VoterPc == null || ClientPc == null)
+        {
+            Logger.Info($"Client {srcClient} add kick vote for Target {clientId}, but one of them has null PlayerControl", "VoteBanSystem");
+            return false;
+        }
+
+        Logger.Info($"Client {srcClient} - Player {VoterPc.GetNameWithRole()} add kick vote for Client {clientId} - TargetPc {ClientPc.GetNameWithRole()}", "VoteBanSystem");
+        if (ClientPc.Data.Disconnected)
+        {
+            Logger.Info($"Canceled because target disconnected", "VoteBanSystem");
+            return false;
+        }
+
+        if (DevManager.GetDevUser(ClientPc.FriendCode).DeBug && ClientPc.AmOwner)
+        {
+            Logger.Info($"Canceled because target is dev host", "VoteBanSystem");
+            return false; //If the host has debug permission, Prevent host from being vote kicked
+        }
+
+        return true;
+    }
+}
