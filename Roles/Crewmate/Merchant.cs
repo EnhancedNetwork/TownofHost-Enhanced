eb44df232ecf69c57dcbb6ef26c29e80d35b8111
Drakos <143632280+Ultradragon005@users.noreply.github.com>
2024-01-19 19:35:28 +0100

    Update Merchant.cs

--- Roles/Crewmate/Merchant.cs
+++ Roles/Crewmate/Merchant.cs
@@ -200,6 +200,8 @@ namespace TOHE.Roles.Crewmate
                 target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Merchant), GetString("MerchantAddonSell")));
                 player.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Merchant), GetString("MerchantAddonDelivered")));
 
+                Tired.playerIdList.Add(target.PlayerId, false);
+                
                 addonsSold[player.PlayerId] += 1;
             }
         }
