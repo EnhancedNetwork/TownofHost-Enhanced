f6f0a45a29d5746c34966b4056e66cd0e04e8e50
TommyXL <104814436+Tommy-XL@users.noreply.github.com>
2024-01-24 21:46:03 +0800

    Update OptionHolder.cs

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -1088,7 +1088,7 @@ public static class Options
     public static void Load()
     {
         //#######################################
-        // 27600 lasted id for roles/add-ons (Next use 27700)
+        // 27300 lasted id for roles/add-ons (Next use 27400)
         // Limit id for  roles/add-ons --- "59999"
         //#######################################
 
