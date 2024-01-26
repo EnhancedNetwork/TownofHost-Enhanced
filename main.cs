665bc9fc9d988bc589e022962d258236916b6178
Moe <moebstergaming@hotmail.com>
2024-01-25 19:20:00 -0500

    fixed number again tehe

--- main.cs
+++ main.cs
@@ -40,7 +40,7 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.1.24.151021";
+    public const string PluginVersion = "2024.1.24.15121";
     public const string PluginDisplayVersion = "1.5.1 Dev 2.1";
     public static readonly string SupportedVersionAU = "2023.10.24"; // also 2023.11.28
 
