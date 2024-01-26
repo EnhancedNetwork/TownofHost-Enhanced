befd5dfb72673a2f9ed0202a9b09031871a1dfe2
Tommy XL <fadeevvladimir16@gmail.com>
2024-01-26 21:58:55 +0800

    Remove role.IsConverted() from IsCrewmateTeamV2()

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -1772,7 +1772,7 @@ static class CustomRolesHelper
     public static bool IsImpostorTeamV2(this CustomRoles role) => (role.IsImpostorTeamV3() && role != CustomRoles.Trickster && !role.IsConverted()) || role == CustomRoles.Rascal || role == CustomRoles.Madmate;
     public static bool IsNeutralTeamV2(this CustomRoles role) => (role.IsConverted() || role.IsNeutral()) && role != CustomRoles.Madmate;
 
-    public static bool IsCrewmateTeamV2(this CustomRoles role) => !(role.IsImpostorTeamV2() || role.IsNeutralTeamV2() || role.IsConverted()) || role == CustomRoles.Trickster;
+    public static bool IsCrewmateTeamV2(this CustomRoles role) => !(role.IsImpostorTeamV2() || role.IsNeutralTeamV2()) || role == CustomRoles.Trickster;
 
     public static bool IsConverted(this CustomRoles role)
     {
