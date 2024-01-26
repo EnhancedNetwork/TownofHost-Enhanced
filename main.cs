c261f2eeef3151c37b71cff073e22fc2106e1db4
Moe <moebstergaming@hotmail.com>
2024-01-25 19:09:41 -0500

    Change version

--- main.cs
+++ main.cs
@@ -40,8 +40,8 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.1.24.1502";
-    public const string PluginDisplayVersion = "1.5.0 Canary 2";
+    public const string PluginVersion = "2024.1.24.15101";
+    public const string PluginDisplayVersion = "1.5.1 Dev 2";
     public static readonly string SupportedVersionAU = "2023.10.24"; // also 2023.11.28
 
     /******************* Change one of the three variables to true before making a release. *******************/
