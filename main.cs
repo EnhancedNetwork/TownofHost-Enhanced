fc7614013a032fd74e42becdb9df89ac9d2db2fe
TommyXL <104814436+Tommy-XL@users.noreply.github.com>
2024-02-06 10:39:36 +0800

    Change Plugin Version

--- main.cs
+++ main.cs
@@ -40,7 +40,7 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.0204.150.0000"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginVersion = "2024.0206.150.0000"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "1.5.0";
     public static readonly string SupportedVersionAU = "2023.10.24"; // also 2023.11.28
 
