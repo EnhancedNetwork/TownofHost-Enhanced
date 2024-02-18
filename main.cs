ac8953302bba980226eedd2711fea7550a97e6b8
Tommy XL <fadeevvladimir16@gmail.com>
2024-02-18 17:03:52 +0800

    Plugin Version

--- main.cs
+++ main.cs
@@ -40,7 +40,7 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.0216.160.0001"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginVersion = "2024.0218.160.0001"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "1.6.0 dev 1";
     public static readonly string SupportedVersionAU = "2023.10.24"; // also 2023.11.28
 
