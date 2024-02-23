f02652132c6b008639b3b83fda8f55d372bdd355
ryuk201198 <138619164+ryuk201198@users.noreply.github.com>
2024-02-23 20:48:08 +0530

    seeker complete

--- Modules/AntiBlackout.cs
+++ Modules/AntiBlackout.cs
@@ -231,7 +231,6 @@ public static class AntiBlackout
             if (Eraser.IsEnable) Eraser.AfterMeetingTasks(notifyPlayer: true);
             if (Cleanser.IsEnable) Cleanser.AfterMeetingTasks(notifyPlayer: true);
             if (Vulture.IsEnable) Vulture.AfterMeetingTasks(notifyPlayer: true);
-            if (Seeker.IsEnable) Seeker.AfterMeetingTasks(notifyPlayer: true);
 
         }, timeNotify + 0.2f, "Notify AfterMeetingTasks");
     }
