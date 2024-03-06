72e4da9c38d7306dc00f6eac5ec01c81579eef3e
Tommy XL <fadeevvladimir16@gmail.com>
2024-03-06 12:20:38 +0800

    AprilFoolsModePatch

---
+++ Patches/AprilFoolsModePatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace TOHE.Patches;
+
+//[HarmonyPatch(typeof(AprilFoolsMode), nameof(AprilFoolsMode.ShouldLongAround))]
+//class AprilFoolsModePatch
+//{
+//    public static bool Prefix(ref bool __result)
+//    {
+//        __result = true;
+//        return false;
+//    }
+//}
