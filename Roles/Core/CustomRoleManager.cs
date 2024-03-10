afa867575cb6dc19c01849c6bf6e9732d457150c
Tommy XL <fadeevvladimir16@gmail.com>
2024-03-10 18:22:10 +0800

    Fix Alchemist

--- Roles/Core/CustomRoleManager.cs
+++ Roles/Core/CustomRoleManager.cs
@@ -117,7 +117,7 @@ public static class CustomRoleManager
         // ==== Crewmates ====
         CustomRoles.Addict => new Addict(),
         //ustomRoles.Admirer => new Admirer(),
-        //CustomRoles.Alchemist => new Alchemist(),
+        CustomRoles.Alchemist => new Alchemist(),
         CustomRoles.Bastion => new Bastion(),
         CustomRoles.Benefactor => new Benefactor(),
         CustomRoles.Bodyguard => new Bodyguard(),
