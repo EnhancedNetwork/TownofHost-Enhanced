c33f85ee5927a6cb4950227f5350703c2d9a4886
Tommy XL <fadeevvladimir16@gmail.com>
2024-03-16 13:39:38 +0800

    Show Role Summary in logs after game end

--- Patches/OutroPatch.cs
+++ Patches/OutroPatch.cs
@@ -338,6 +338,8 @@ class SetEverythingUpPatch
         RoleSummaryRectTransform.anchoredPosition = new Vector2(Pos.x + 3.5f, Pos.y - 0.1f);
         RoleSummary.text = sb.ToString();
 
+        Logger.Info($"{RoleSummary.text.RemoveHtmlTags()}", "Role Summary");
+
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         //Utils.ApplySuffix();
