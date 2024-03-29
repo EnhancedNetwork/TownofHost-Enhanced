643d5d9af35f508d54dc4f395bedb3c3112da371
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-03-29 12:56:33 +0100

    fix

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1384,7 +1384,7 @@ public static class Utils
     }
     public static List<PlayerControl> GetPlayerListByIds(this IEnumerable<byte> PlayerIdList)
     {
-        return PlayerIdList.Select(x => GetPlayerById(x)).ToList();
+        return PlayerIdList.ToList().Select(x => GetPlayerById(x)).ToList();
     }
     public static GameData.PlayerInfo GetPlayerInfoById(int PlayerId) =>
         GameData.Instance.AllPlayers.ToArray().FirstOrDefault(info => info.PlayerId == PlayerId);
