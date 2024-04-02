773cd908a72e37ac9c14af17dccce27bf9980e79
Tommy XL <fadeevvladimir16@gmail.com>
2024-04-02 12:57:09 +0800

    fucking using static UnityEngine.GraphicsBuffer and MS.Internal.Xml.XPath

--- Roles/Core/CustomRoleManager.cs
+++ Roles/Core/CustomRoleManager.cs
@@ -1,5 +1,4 @@
 ﻿using AmongUs.GameOptions;
-using MS.Internal.Xml.XPath;
 using System;
 using System.Text;
 using TOHE.Roles.AddOns.Common;
@@ -8,7 +7,6 @@ using TOHE.Roles.AddOns.Impostor;
 using TOHE.Roles.Crewmate;
 using TOHE.Roles.Impostor;
 using TOHE.Roles.Neutral;
-using static UnityEngine.GraphicsBuffer;
 
 namespace TOHE.Roles.Core;
 
