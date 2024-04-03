a5b8f0ff23c49144a270ec96f75bb11397d2145a
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-04-03 21:36:06 +0200

    change

--- Roles/Core/CustomRoleManager.cs
+++ Roles/Core/CustomRoleManager.cs
@@ -13,7 +13,7 @@ namespace TOHE.Roles.Core;
 public static class CustomRoleManager
 {
     public static readonly Dictionary<CustomRoles, RoleBase> RoleClass = [];
-    public static RoleBase GetRoleClass(this CustomRoles role) => RoleClass[role]; // Assigned in main, and already defaults to new vanilla role if null; Other-wise throw exception.
+    public static RoleBase GetRoleClass(this CustomRoles role) => RoleClass.TryGetValue(role, out var roleClass) & roleClass != null ? roleClass : new VanillaRole(); // Main alr assigns it new vanilla if null, but incase it is addon somehow O-o
     public static List<RoleBase> AllEnabledRoles => RoleClass.Values.Where(x => x.IsEnable).ToList();
     public static bool HasEnabled(this CustomRoles role) => RoleClass[role].IsEnable;
     public static RoleBase GetRoleClass(this PlayerControl player) => GetRoleClassById(player.PlayerId);
