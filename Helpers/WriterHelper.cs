e8b85054eee148581d712667c9c3570c6940dd6e
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-04-26 21:15:43 -0300

    Add files via upload

---
+++ Helpers/WriterHelper.cs
@@ -0,0 +1,22 @@
+﻿using Hazel;
+
+namespace TOHE;
+
+public static class WriterHelper
+{
+    /// <summary>
+    /// Starts a rpc using a integer value.
+    /// </summary>
+    /// <param name="targetNetId">The target InnerNetObject that the rpc will be executed on</param>
+    /// <param name="callId">The call that will be called in the InnerNetObject</param>
+    /// <param name="option">The send option of the packet, Reliable packets are bigger</param>
+    /// <returns>A new MessageWriter</returns>
+    public static MessageWriter StartRpc(this AmongUsClient client, uint targetNetId, int callId, SendOption option = SendOption.Reliable)
+    {
+        MessageWriter messageWriter = client.Streams[(int)option];
+        messageWriter.StartMessage(2);
+        messageWriter.WritePacked(targetNetId);
+        messageWriter.Write(callId);
+        return messageWriter;
+    }
+}
