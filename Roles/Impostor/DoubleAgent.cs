0c493a32fcd4757be4035d8b1a87a6d368a40ed2
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-05-15 02:35:15 +0000

    Made DoubleAgent Madmate!

--- Roles/Impostor/DoubleAgent.cs
+++ Roles/Impostor/DoubleAgent.cs
@@ -20,7 +20,7 @@ internal class DoubleAgent : RoleBase
     public static bool HasEnabled => playerIdList.Any();
     public override bool IsEnable => HasEnabled;
     public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
-    public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorSupport;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.Madmate;
     //==================================================================\\
 
     public static OptionItem DoubleAgentCanDiffuseBombs;
