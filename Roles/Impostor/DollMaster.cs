200f96c4990fbffe8e489c993c886bc73a8858c1
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-05-18 11:37:38 +0000

    Set DollMaster back to a single role

--- Roles/Impostor/DollMaster.cs
+++ Roles/Impostor/DollMaster.cs
@@ -36,7 +36,7 @@ internal class DollMaster : RoleBase
 
     public override void SetupCustomOption()
     {
-        SetupRoleOptions(Id, TabGroup.OtherRoles, CustomRoles.DollMaster);
+        SetupSingleRoleOptions(Id, TabGroup.OtherRoles, CustomRoles.DollMaster);
         DefaultKillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(0f, 180f, 2.5f), 25f, TabGroup.OtherRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DollMaster])
             .SetValueFormat(OptionFormat.Seconds);
         ShapeshiftCooldown = FloatOptionItem.Create(Id + 11, "DollMasterPossessionCooldown", new(0f, 180f, 2.5f), 25f, TabGroup.OtherRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DollMaster])
