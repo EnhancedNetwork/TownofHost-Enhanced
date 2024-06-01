ffa95ec02c2c52d6c31358c53ab5094e6999d32e
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-06-01 17:15:20 +0200

    nvm wtf is this

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -266,8 +266,6 @@ static class ExtendedPlayerControl
             player.RpcSetHat(Outfit.HatId);
             player.RpcSetSkin(Outfit.SkinId);
             player.RpcSetVisor(Outfit.VisorId);
-            player.RpcSetPet(Outfit.PetId);
-            player.RpcSetNamePlate(Outfit.NamePlateId);
 
             //cannot use currentoutfit type because of mushroom mixup . .
             var OutfitTypeSet = player.CurrentOutfitType != PlayerOutfitType.Shapeshifted ? PlayerOutfitType.Default : PlayerOutfitType.Shapeshifted;
