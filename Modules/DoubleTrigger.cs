72bae9c633ff2787931d9e02204112744db427d2
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-06-05 15:03:58 +0800

    remove logging

--- Modules/DoubleTrigger.cs
+++ Modules/DoubleTrigger.cs
@@ -67,7 +67,6 @@ static class DoubleTrigger
         if (!FirstTriggerTimer.ContainsKey(playerId)) return;
 
         FirstTriggerTimer[playerId] -= Time.fixedDeltaTime;
-        Logger.Info($"{player.name} FirstTriggerTimer: {FirstTriggerTimer[playerId]}", "DoubleTrigger");
         if (FirstTriggerTimer[playerId] <= 0)
         {
             Logger.Info($"{player.name} Do single action", "DoubleTrigger");
