6487a73bfcffe8df189374ab5f529a830bfcd4cc
Tommy XL <fadeevvladimir16@gmail.com>
2024-06-07 00:59:38 +0800

    Spy and Fragile

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -594,7 +594,8 @@ public static class CustomRolesHelper
                     || pc.Is(CustomRoles.Masochist)
                     || pc.Is(CustomRoles.SchrodingersCat)
                     || pc.Is(CustomRoles.PlagueBearer)
-                    || pc.Is(CustomRoles.Pestilence))
+                    || pc.Is(CustomRoles.Pestilence)
+                    || pc.Is(CustomRoles.Spy))
                     return false;
                 if ((pc.GetCustomRole().IsCrewmate() && !Fragile.CrewCanBeFragile.GetBool()) || (pc.GetCustomRole().IsNeutral() && !Fragile.NeutralCanBeFragile.GetBool()) || (pc.GetCustomRole().IsImpostor() && !Fragile.ImpCanBeFragile.GetBool()))
                     return false;
