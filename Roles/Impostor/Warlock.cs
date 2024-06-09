05062aa558c3b39f412d562e4db3fee38d447369
Pietrodjaowjao <nicoleobenetti@gmail.com>
2024-06-09 09:49:20 -0300

    xing xong

--- Roles/Impostor/Warlock.cs
+++ Roles/Impostor/Warlock.cs
@@ -31,7 +31,7 @@ internal class Warlock : RoleBase
     public override void SetupCustomOption()
     {
         Options.SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.Warlock);
-        WarlockCanKillAllies = BooleanOptionItem.Create(Id + 2, "CanKillImpostors", true, TabGroup.ImpostorRoles, false)
+        WarlockCanKillAllies = BooleanOptionItem.Create("CanKillImpostors", true, TabGroup.ImpostorRoles, false)
             .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Warlock]);
         WarlockCanKillSelf = BooleanOptionItem.Create("Warlock_CanKillSelf", false, TabGroup.ImpostorRoles, false)
             .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Warlock]);
