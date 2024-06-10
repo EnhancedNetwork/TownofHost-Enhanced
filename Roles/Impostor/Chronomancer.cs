a2414770353cf4cdc63b0dffddfb3baa69e30d90
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-06-10 17:43:31 +0200

    fix formatting chronomancer

--- Roles/Impostor/Chronomancer.cs
+++ Roles/Impostor/Chronomancer.cs
@@ -44,7 +44,7 @@ internal class Chronomancer : RoleBase
         Dtime = FloatOptionItem.Create(Id + 11, "ChronomancerDecreaseTime", new(0.05f, 1f, 0.05f), 0.15f, TabGroup.ImpostorRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Chronomancer])
             .SetValueFormat(OptionFormat.Seconds);
         ReduceVision = FloatOptionItem.Create(Id + 12, "ChronomancerVisionMassacre", new(0.25f, 1f, 0.25f), 0.5f, TabGroup.ImpostorRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Chronomancer])
-            .SetValueFormat(OptionFormat.Seconds);
+            .SetValueFormat(OptionFormat.Multiplier);
     }
     public override void Add(byte playerId)
     {
