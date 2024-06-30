9366c9755757e585d80f4c474d000fdb11411ca6
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-06-30 11:58:14 +0800

    更新 PlayerJoinAndLeftPatch.cs

--- Patches/PlayerJoinAndLeftPatch.cs
+++ Patches/PlayerJoinAndLeftPatch.cs
@@ -223,7 +223,7 @@ public static class OnPlayerJoinedPatch
                 }
             }
             catch { }
-        }, 2.5f, "green bean kick late task", false);
+        }, 3f, "green bean kick late task", false);
 
 
         if (AmongUsClient.Instance.AmHost && HasInvalidFriendCode(client.FriendCode) && Options.KickPlayerFriendCodeInvalid.GetBool() && !GameStates.IsLocalGame)
