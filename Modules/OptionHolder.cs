dc39ca071683805dd0a63b9eaccbd33749e9f7d7
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-09 13:58:34 +0800

    Fix fatal error when game loading

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -22,19 +22,23 @@ public enum CustomGameMode
 [HarmonyPatch]
 public static class Options
 {
-    static Task taskOptionsLoad;
     [HarmonyPatch(typeof(TranslationController), nameof(TranslationController.Initialize)), HarmonyPostfix]
     public static void OptionsLoadStart_Postfix()
     {
         Logger.Msg("Mod option loading start", "Load Options");
-        taskOptionsLoad = Task.Run(Load);
+        try
+        {
+            Load();
+        }
+        catch (Exception error)
+        {
+            Logger.Error($"Fatal error after loading mad options: {error}", "Load Options");
+        }
+        finally
+        {
+            Logger.Msg("Mod option loading eng", "Load Options");
+        }
         //taskOptionsLoad.ContinueWith(t => { Logger.Msg("Mod option loading end", "Load Options"); });
-    }
-    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start)), HarmonyPostfix]
-    public static void WaitOptionsLoad_Postfix()
-    {
-        taskOptionsLoad.Wait();
-        Logger.Msg("Mod option loading eng", "Load Options");
     }
 
     // Presets
