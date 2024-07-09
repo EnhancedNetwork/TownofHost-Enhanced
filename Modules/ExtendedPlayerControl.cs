00f0451878c0f5ebb7a6b182cdd7fef301c05234
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-09 18:25:12 +0800

    Use +20

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -1143,7 +1143,7 @@ static class ExtendedPlayerControl
         if (player == null) return;
         var netTransform = player.NetTransform;
         var clientId = seer.GetClientId();
-        ushort addSid = GameStates.IsLocalGame ? (ushort)4 : (ushort)328;
+        ushort addSid = GameStates.IsLocalGame ? (ushort)4 : (ushort)20;
         if (AmongUsClient.Instance.ClientId == clientId)
         {
             netTransform.SnapTo(position, (ushort)(netTransform.lastSequenceId + addSid));
