72eb2048e5dccc49fa30fa865fe9e18fbd2b3140
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-13 15:46:07 +0800

    Add logs

--- Patches/OutroPatch.cs
+++ Patches/OutroPatch.cs
@@ -351,6 +351,7 @@ class SetEverythingUpPatch
         RoleSummaryRectTransform.anchoredPosition = new Vector2(Pos.x + 3.5f, Pos.y - 0.1f);
         RoleSummary.text = sb.ToString();
 
+        Logger.Info($"{CustomWinnerHolder.WinnerTeam}", "Winner Team");
         Logger.Info($"{LastWinsReason}", "Wins Reason");
         Logger.Info($"{RoleSummary.text.RemoveHtmlTags()}", "Role Summary");
 
