3a917bded92bd6579c1e3c5a3992f3e86c19b8dd
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-15 22:16:19 +0800

    Add SetHudActive(true) in LateTask

--- Patches/ExilePatch.cs
+++ Patches/ExilePatch.cs
@@ -200,8 +200,11 @@ class ExileControllerWrapUpPatch
         RemoveDisableDevicesPatch.UpdateDisableDevices();
         SoundManager.Instance.ChangeAmbienceVolume(DataManager.Settings.Audio.AmbienceVolume);
 
-        if (!AmongUsClient.Instance.IsGameOver)
-            DestroyableSingleton<HudManager>.Instance.SetHudActive(true);
+        _ = new LateTask(() =>
+        {
+            if (!AmongUsClient.Instance.IsGameOver)
+                DestroyableSingleton<HudManager>.Instance.SetHudActive(true);
+        }, 0.8f, "Set Hud Active");
 
         Logger.Info("Start of Task Phase", "Phase");
     }
