ed5e4fe1dc1c69b7ecbb5dad331fea9b9f0530b7
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-21 01:14:35 +0800

    Canary 11

--- main.cs
+++ main.cs
@@ -39,12 +39,12 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.0717.200.10100"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.0.0 Canary 10 Hotfix 1";
+    public const string PluginVersion = "2024.0721.200.11000"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.0.0 Canary 11";
     public const string SupportedVersionAU = "2024.6.18";
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool Canary = true; // ACTIVE - Latest: V2.0.0 10 Hotfix 1
+    public static readonly bool Canary = true; // ACTIVE - Latest: V2.0.0 11
     public static readonly bool fullRelease = false; // INACTIVE - Latest: V1.6.0
     public static readonly bool devRelease = false; // INACTIVE - Latest: V2.0.0 Dev 25
 
