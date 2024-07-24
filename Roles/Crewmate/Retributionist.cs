037298612d508f099925af84c0e700bcbd96f6a6
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-24 14:23:28 +0800

    Change custom button for Retributionist and Nemesis

--- Roles/Crewmate/Retributionist.cs
+++ Roles/Crewmate/Retributionist.cs
@@ -230,9 +230,9 @@ internal class Retributionist : RoleBase
             GameObject template = pva.Buttons.transform.Find("CancelButton").gameObject;
             GameObject targetBox = UnityEngine.Object.Instantiate(template, pva.transform);
             targetBox.name = "ShootButton";
-            targetBox.transform.localPosition = new Vector3(-0.95f, 0.03f, -1.31f);
+            targetBox.transform.localPosition = new Vector3(-0.35f, 0.03f, -1.31f);
             SpriteRenderer renderer = targetBox.GetComponent<SpriteRenderer>();
-            renderer.sprite = CustomButton.Get("TargetIcon");
+            renderer.sprite = CustomButton.Get("MeetingKillButton");
             PassiveButton button = targetBox.GetComponent<PassiveButton>();
             button.OnClick.RemoveAllListeners();
             button.OnClick.AddListener((Action)(() => RetributionistOnClick(pva.TargetPlayerId/*, __instance*/)));
