46441c334c172e4546fdd6cf5f38ecb11c159dba
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-31 17:03:37 +0800

    Some change

--- Roles/AddOns/Impostor/Madmate.cs
+++ Roles/AddOns/Impostor/Madmate.cs
@@ -83,7 +83,7 @@ public static class Madmate
 
     public static bool CanBeMadmate(this PlayerControl pc, bool forAdmirer = false, bool forGangster = false)
     {
-        return pc != null && (pc.GetCustomRole().IsCrewmate() || (pc.GetCustomRole().IsNeutral() && forAdmirer)) && !pc.Is(CustomRoles.Madmate)
+        return pc != null && (pc.GetCustomRole().IsCrewmate() || (forAdmirer && pc.GetCustomRole().IsNeutral())) && !pc.Is(CustomRoles.Madmate)
         && !(pc.CheckCanBeMadmate(forGangster) ||
             pc.Is(CustomRoles.LazyGuy) ||
             pc.Is(CustomRoles.Lazy) ||
