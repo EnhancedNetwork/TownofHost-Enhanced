088a831e981501e2e61445a21ec53f9ee5143d82
Tommy XL <fadeevvladimir16@gmail.com>
2024-07-31 16:54:24 +0800

    Some fix

--- Patches/MeetingHudPatch.cs
+++ Patches/MeetingHudPatch.cs
@@ -67,24 +67,33 @@ class CheckForEndVotingPatch
                     });
                     states = [.. statesList];
 
-                    ExileControllerWrapUpPatch.AntiBlackout_LastExiled = voteTarget.Data;
+                    var exiled = voteTarget.Data;
+
+                    ExileControllerWrapUpPatch.AntiBlackout_LastExiled = exiled;
+                    Main.LastVotedPlayerInfo = exiled;
 
                     if (AntiBlackout.BlackOutIsActive)
                     {
                         // Need check BlackOutIsActive again
                         if (AntiBlackout.BlackOutIsActive)
-                            __instance.AntiBlackRpcVotingComplete(states, voteTarget.Data, false);
+                            __instance.AntiBlackRpcVotingComplete(states, exiled, false);
+                        else
+                            __instance.RpcVotingComplete(states, exiled, false);
 
-                        AntiBlackout.ShowExiledInfo = true;
-                        ConfirmEjections(voteTarget.Data, true);
+                        if (exiled != null)
+                        {
+                            AntiBlackout.ShowExiledInfo = true;
+                            ConfirmEjections(exiled, true);
+                        }
                     }
                     else
                     {
-                        __instance.RpcVotingComplete(states, voteTarget.Data, false);
+                        __instance.RpcVotingComplete(states, exiled, false);
 
-                        Main.LastVotedPlayerInfo = voteTarget.Data;
-                        if (Main.LastVotedPlayerInfo != null)
-                            ConfirmEjections(Main.LastVotedPlayerInfo);
+                        if (exiled != null)
+                        {
+                            ConfirmEjections(exiled);
+                        }
                     }
 
                     Logger.Info($"{voteTarget.GetNameWithRole()} expelled by Dictator", "Dictator");
@@ -342,15 +351,16 @@ class CheckForEndVotingPatch
             exiledPlayer?.Object.SetRealKiller(null);
 
             ExileControllerWrapUpPatch.AntiBlackout_LastExiled = exiledPlayer;
+            Main.LastVotedPlayerInfo = exiledPlayer;
 
             //RPC
             if (AntiBlackout.BlackOutIsActive)
             {
-                //__instance.RpcVotingComplete(states, null, true);
-
                 // Need check BlackOutIsActive again
                 if (AntiBlackout.BlackOutIsActive)
                     __instance.AntiBlackRpcVotingComplete(states, exiledPlayer, tie);
+                else
+                    __instance.RpcVotingComplete(states, exiledPlayer, tie);
 
                 if (exiledPlayer != null)
                 {
@@ -361,11 +371,10 @@ class CheckForEndVotingPatch
             else
             {
                 __instance.RpcVotingComplete(states, exiledPlayer, tie); // Normal processing
-                
-                Main.LastVotedPlayerInfo = exiledPlayer;
-                if (Main.LastVotedPlayerInfo != null)
+
+                if (exiledPlayer != null)
                 {
-                    ConfirmEjections(Main.LastVotedPlayerInfo);
+                    ConfirmEjections(exiledPlayer);
                 }
             }
 
