c0bed6c06abbe616de8b496c06330eb77ecfbe93
D1GQ <83262852+D1GQ@users.noreply.github.com>
2024-08-11 04:31:59 -0500

    maxColumns

--- Patches/RegionMenuPatch.cs
+++ Patches/RegionMenuPatch.cs
@@ -9,13 +9,21 @@ public static class RegionMenuPatch
     [HarmonyPostfix]
     public static void AdjustButtonPositions_Postfix(RegionMenu __instance)
     {
-        const int buttonsPerColumn = 6;
+        const int maxColumns = 4;
+        int buttonsPerColumn = 6;
         float buttonSpacing = 0.6f;
         float buttonSpacingSide = 2.25f;
 
         List<UiElement> buttons = __instance.controllerSelectable.ToArray().ToList();
 
         int columnCount = (buttons.Count + buttonsPerColumn - 1) / buttonsPerColumn;
+
+        while (columnCount > maxColumns)
+        {
+            buttonsPerColumn++;
+            columnCount = (buttons.Count + buttonsPerColumn - 1) / buttonsPerColumn;
+        }
+
         float totalWidth = (columnCount - 1) * buttonSpacingSide;
         float totalHeight = (buttonsPerColumn - 1) * buttonSpacing;
 
