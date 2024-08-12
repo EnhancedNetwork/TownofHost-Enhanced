23002ffa237407da2832bef330e22d5dab6a23ca
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-08-12 19:24:36 +0200

    change id

--- Roles/AddOns/Common/Messenger.cs
+++ Roles/AddOns/Common/Messenger.cs
@@ -11,7 +11,7 @@ namespace TOHE.Roles.AddOns.Common;
 
 public class Messenger : IAddon
 {
-    private const int Id = 29000;
+    private const int Id = 29100;
     private static OptionItem ImpostorsHearMessage;
     private static OptionItem NeutralsHearMessage;
     private static OptionItem KnowMessenger;
