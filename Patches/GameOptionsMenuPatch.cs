5783369c32351ed0a8420dfcedcb95cc00eca9a0
Tommy XL <fadeevvladimir16@gmail.com>
2024-08-16 23:04:13 +0800

    Fix bug

--- Patches/GameOptionsMenuPatch.cs
+++ Patches/GameOptionsMenuPatch.cs
@@ -629,10 +629,10 @@ public static class StringOptionPatch
                 if (Enum.GetValues<CustomRoles>().Find(x => GetString($"{x}") == name.RemoveHtmlTags(), out var role))
                 {
                     var roleName = role.IsVanilla() ? role + "TOHE" : role.ToString();
-                    var str = Translator.GetString($"{roleName}InfoLong");
+                    var str = GetString($"{roleName}InfoLong");
                     int Lenght = str.Length > 360 ? 360 : str.Length;
                     var infoLong = str[(str.IndexOf('\n') + 1)..Lenght];
-                    var ColorRole = Utils.ColorString(Utils.GetRoleColor(role), role.ToString());
+                    var ColorRole = Utils.ColorString(Utils.GetRoleColor(role), GetString(role.ToString()));
                     var info = $"<size=70%>{ColorRole}: {infoLong}</size>";
                     GameSettingMenu.Instance.MenuDescriptionText.text = info;
                 }
