1848490283b52160ff35b9e9a15729ef0b29e301
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-08-17 20:00:52 +0200

    add more

--- Roles/AddOns/Common/Rebirth.cs
+++ Roles/AddOns/Common/Rebirth.cs
@@ -3,6 +3,7 @@ using static TOHE.Utils;
 using static TOHE.Translator;
 using TOHE.Modules;
 using static UnityEngine.GraphicsBuffer;
+using TOHE.Roles.Neutral;
 
 namespace TOHE.Roles.AddOns.Common;
 
@@ -33,7 +34,8 @@ public class Rebirth : IAddon
         if (!pc.Is(CustomRoles.Rebirth)) return false;
         var ViablePlayer = Main.AllAlivePlayerControls.Where(x => x != pc).Shuffle(IRandom.Instance)
             .FirstOrDefault(x => x != null && !x.OwnedByHost() && !x.IsAnySubRole(x => x.IsConverted()) && !x.Is(CustomRoles.Admired) && !x.Is(CustomRoles.Knighted) && 
-/*All converters */    (!x.Is(CustomRoles.Cultist) && !x.Is(CustomRoles.Infected) && !x.Is(CustomRoles.Virus) && !x.Is(CustomRoles.Jackal) && !x.Is(CustomRoles.Admirer)));
+/*All converters */    (!x.Is(CustomRoles.Cultist) && !x.Is(CustomRoles.Infected) && !x.Is(CustomRoles.Virus) && !x.Is(CustomRoles.Jackal) && !x.Is(CustomRoles.Admirer)) &&
+                !x.Is(CustomRoles.Lovers) && x.Is(CustomRoles.Romantic) && !x.GetCustomRole().IsImpostor());
 
         if (ViablePlayer == null)
         {
