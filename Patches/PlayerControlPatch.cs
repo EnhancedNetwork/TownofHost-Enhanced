9de3a409ae8b304890e5a45fcac68ba761ce03a0
Ultradragon005 <143632280+Ultradragon005@users.noreply.github.com>
2024-08-20 19:54:27 +0200

    BRUV

--- Patches/PlayerControlPatch.cs
+++ Patches/PlayerControlPatch.cs
@@ -613,7 +613,7 @@ public static class CheckShapeshiftPatch
         if (instance == target && Main.UnShapeShifter.Contains(instance.PlayerId))
         {
             if (!instance.IsMushroomMixupActive() && !GameStates.IsMeeting) instance.GetRoleClass().UnShapeShiftButton(instance);
-            instance.RpcResetAbilityCooldown(); // jsut incase
+            instance.RpcResetAbilityCooldown(); // just incase
             logger.Info($"Cancel shapeshifting because {instance.GetRealName()} is using un-shapeshift ability button");
             return false;
         }
