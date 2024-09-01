81416c7b923c3d5b4a78a831f0bb2e512fc89099
Tommy XL <fadeevvladimir16@gmail.com>
2024-09-02 01:54:05 +0800

    Hotfix 1

--- main.cs
+++ main.cs
@@ -41,12 +41,12 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.0831.210.00080"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.1.0 Alpha 8";
+    public const string PluginVersion = "2024.0831.210.00081"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.1.0 Alpha 8 Hotfix 1";
     public const string SupportedVersionAU = "2024.8.13";
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool devRelease = true; // Latest: V2.1.0 Alpha 8
+    public static readonly bool devRelease = true; // Latest: V2.1.0 Alpha 8 Hotfix 1
     public static readonly bool canaryRelease = false; // Latest: V2.0.0 Canary 12
     public static readonly bool fullRelease = false; // Latest: V2.0.3
 
