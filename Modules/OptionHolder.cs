a2ea1a3ddf81deeb52589eae72446244cdc4a6f2
Pyro <btriche.sots@gmail.com>
2024-09-04 00:50:22 -0400

    revert id

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -621,7 +621,7 @@ public static class Options
     private static System.Collections.IEnumerator CoLoadOptions()
     {
         //#######################################
-        // 29800 last id for roles/add-ons (Next use 29900)
+        // 29700 last id for roles/add-ons (Next use 29800)
         // Limit id for roles/add-ons --- "59999"
         //#######################################
 
