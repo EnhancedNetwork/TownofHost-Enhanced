9c2d69bd33c6289609e705b7bb02ad8b8731ba3a
Marg <51059123+MargaretTheFool@users.noreply.github.com>
2024-09-19 17:03:17 -0400

    fix

--- Roles/Neutral/Terrorist.cs
+++ Roles/Neutral/Terrorist.cs
@@ -55,7 +55,7 @@ internal class Terrorist : RoleBase
     private static void CheckTerroristWin(NetworkedPlayerInfo terrorist)
     {
         var taskState = Utils.GetPlayerById(terrorist.PlayerId).GetPlayerTaskState();
-        if (taskState.IsTaskFinished && (!Main.PlayerStates[terrorist.PlayerId].IsSuicide || CanTerroristSuicideWin.GetBool()))
+        if (taskState.IsTaskFinished && (!Main.PlayerStates[terrorist.PlayerId].IsSuicide || CanTerroristSuicideWin.GetBool()) && (Main.PlayerStates[terrorist.PlayerId].deathReason != PlayerState.DeathReason.Armageddon))
         {
             if (!CustomWinnerHolder.CheckForConvertedWinner(terrorist.PlayerId))
             {
@@ -66,7 +66,7 @@ internal class Terrorist : RoleBase
             {
                 if (pc.Is(CustomRoles.Terrorist))
                 {
-                    if (Main.PlayerStates[pc.PlayerId].deathReason == PlayerState.DeathReason.Vote || Main.PlayerStates[pc.PlayerId].deathReason == PlayerState.DeathReason.Armageddon)
+                    if (Main.PlayerStates[pc.PlayerId].deathReason == PlayerState.DeathReason.Vote)
                     {
                         pc.SetDeathReason(PlayerState.DeathReason.etc);
                     }
