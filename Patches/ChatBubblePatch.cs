9e1d757a009b02d1fbe7f73a39f5dee36558507f
Tommy XL <fadeevvladimir16@gmail.com>
2024-10-27 15:23:34 +0800

    Fix Dark Theme in lasted version AU

--- Patches/ChatBubblePatch.cs
+++ Patches/ChatBubblePatch.cs
@@ -35,9 +35,9 @@ class ChatBubbleSetNamePatch
         if (Main.DarkTheme.Value)
         {
             if (isDead)
-                __instance.Background.color = new Color(0.0f, 0.0f, 0.0f, 0.6f);
+                __instance.Background.color = new(0.1f, 0.1f, 0.1f, 0.6f);
             else
-                __instance.Background.color = Color.black;
+                __instance.Background.color = new(0.1f, 0.1f, 0.1f, 1f);
 
             __instance.TextArea.color = Color.white;
         }
