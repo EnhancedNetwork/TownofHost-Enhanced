293bbf972c8abcf17b0123347bc056974633d417
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-11-18 11:00:56 +0800

    Fix Revenant

--- Roles/Neutral/Revenant.cs
+++ Roles/Neutral/Revenant.cs
@@ -5,16 +5,19 @@ namespace TOHE.Roles.Neutral;
 internal class Revenant : RoleBase
 {
     //===========================SETUP================================\\
-    private const int Id = 31000;
+    private const int Id = 30100;
     public static bool HasEnabled => CustomRoleManager.HasEnabled(CustomRoles.Revenant);
 
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralBenign;
     //==================================================================\\
 
+    // private static OptionItem RevenantCanCopyAddons;
     public override void SetupCustomOption()
-    {
+    {   
         Options.SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Revenant);
+        //RevenantCanCopyAddons = BooleanOptionItem.Create(Id + 10, "RevenantCanCopyAddons", false, TabGroup.NeutralRoles, false)
+         //   .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Revenant]);
     }
 
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
@@ -26,6 +29,7 @@ internal class Revenant : RoleBase
 
         target.RpcChangeRoleBasis(role);
         target.RpcSetCustomRole(role);
+        target.GetRoleClass()?.OnAdd(target.PlayerId);
 
         target.Notify(string.Format(GetString("RenevantTargeted"), Utils.GetRoleName(role)));
 
