4640cfe6365cd9118b5e961cf80b54865b2639e6
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-11-29 19:17:14 +0800

    Change Version 220 Alpha 4

--- main.cs
+++ main.cs
@@ -47,12 +47,12 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.1124.220.00031"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.2.0 Alpha 3 Hotfix 1";
+    public const string PluginVersion = "2024.1129.220.00040"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.2.0 Alpha 4";
     public const string SupportedVersionAU = "2024.10.29"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool devRelease = false; // Latest: V2.2.0 Alpha 3
+    public static readonly bool devRelease = false; // Latest: V2.2.0 Alpha 4
     public static readonly bool canaryRelease = true; // Latest: V2.1.0 Beta 3
     public static readonly bool fullRelease = false; // Latest: V2.1.1
 
