88c748c1ecfd02288728698c8fd0662f0a417c8a
ImpyIsLazy <impostoramongus71@gmail.com>
2024-12-06 01:10:30 +0800

    Add files via upload

---
+++ Roles/AddOns/Impostor/Narc.cs
@@ -0,0 +1,27 @@
+﻿using AmongUs.GameOptions;
+using static TOHE.Options;
+
+namespace TOHE.Roles.AddOns.Impostor;
+
+public class Narc : IAddon
+{
+    private const int Id = 31100;
+    public AddonTypes Type => AddonTypes.Misc;
+
+    public static OptionItem MeetingsNeededForWin;
+
+    public void SetupCustomOption()
+    {
+        SetupAdtRoleOptions(Id, CustomRoles.Narc, canSetNum: false, tab: TabGroup.Addons);
+        MeetingsNeededForWin = IntegerOptionItem.Create(Id + 3, "MeetingsNeededForWin", new(0, 10, 1), 5, TabGroup.Addons, false)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.Narc])
+            .SetValueFormat(OptionFormat.Times);
+    }
+    public void Init()
+    { }
+    public void Add(byte playerId, bool gameIsLoading = true)
+    { }
+    public void Remove(byte playerId)
+    { }
+// Impy is the laziest girl I've ever seen.
+}
