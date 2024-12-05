d6be823fabb91930a3860ae44c9ebc89cc7230e2
PEPPERcula <matthew.d.schlecht@gmail.com>
2024-12-05 16:08:35 -0700

    Update Traitor string

--- Roles/Neutral/Traitor.cs
+++ Roles/Neutral/Traitor.cs
@@ -11,7 +11,7 @@ internal class Traitor : RoleBase
     public static bool HasEnabled => playerIdList.Any();
 
     public override bool IsDesyncRole => true;
-    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+    public override CustomRoles ThisRoleBase => LegacyTraitor.GetBool() ? CustomRoles.Shapeshifter : CustomRoles.Impostor;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
     //==================================================================\\
 
@@ -19,7 +19,12 @@ internal class Traitor : RoleBase
     private static OptionItem CanVent;
     private static OptionItem HasImpostorVision;
     private static OptionItem CanUsesSabotage;
-    private static OptionItem KnowMadmate;
+    public static OptionItem KnowMadmate;
+    private static OptionItem LegacyTraitor;
+    private static OptionItem TraitorShapeshiftCD;
+    private static OptionItem TraitorShapeshiftDur;
+
+
 
     public override void SetupCustomOption()
     {
@@ -30,6 +35,14 @@ internal class Traitor : RoleBase
         HasImpostorVision = BooleanOptionItem.Create(Id + 13, GeneralOption.ImpostorVision, true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Traitor]);
         CanUsesSabotage = BooleanOptionItem.Create(Id + 15, GeneralOption.CanUseSabotage, true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Traitor]);
         KnowMadmate = BooleanOptionItem.Create(Id + 16, "TraitorKnowMadmate", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Traitor]);
+        LegacyTraitor = BooleanOptionItem.Create(Id + 17, "LegacyTraitor", false, TabGroup.NeutralRoles, false)
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Traitor]);
+        TraitorShapeshiftCD = FloatOptionItem.Create(Id + 19, GeneralOption.ShapeshifterBase_ShapeshiftCooldown, new(1f, 180f, 1f), 15f, TabGroup.NeutralRoles, false)
+                .SetParent(LegacyTraitor)
+                .SetValueFormat(OptionFormat.Seconds);
+        TraitorShapeshiftDur = FloatOptionItem.Create(Id + 21, GeneralOption.ShapeshifterBase_ShapeshiftDuration, new(1f, 180f, 1f), 30f, TabGroup.NeutralRoles, false)
+                .SetParent(LegacyTraitor)
+                .SetValueFormat(OptionFormat.Seconds);
     }
     public override void Init()
     {
@@ -39,10 +52,15 @@ internal class Traitor : RoleBase
     {
         playerIdList.Add(playerId);
     }
+    public override void ApplyGameOptions(IGameOptions opt, byte playerId)
+    {
+        opt.SetVision(HasImpostorVision.GetBool());
+        AURoleOptions.ShapeshifterCooldown = TraitorShapeshiftCD.GetFloat();
+        AURoleOptions.ShapeshifterDuration = TraitorShapeshiftDur.GetFloat();
+    }
 
     public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
-    public override void ApplyGameOptions(IGameOptions opt, byte id) => opt.SetVision(HasImpostorVision.GetBool());
-    
+
     public override bool CanUseKillButton(PlayerControl pc) => true;
     public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
     public override bool CanUseSabotage(PlayerControl pc) => CanUsesSabotage.GetBool();
@@ -51,7 +69,6 @@ internal class Traitor : RoleBase
     {
         return !(target == killer || target.Is(Custom_Team.Impostor));
     }
-
     public override string PlayerKnowTargetColor(PlayerControl seer, PlayerControl target)
     {
         if (target.Is(Custom_Team.Impostor))
