fc8c3b84f694a513a384bcf55b285853fa88fe25
PEPPERcula <matthew.d.schlecht@gmail.com>
2024-12-05 16:06:49 -0700

    Add Legacy Version setting for Parasite

--- Roles/Impostor/Parasite.cs
+++ Roles/Impostor/Parasite.cs
@@ -9,12 +9,15 @@ internal class Parasite : RoleBase
     private const int Id = 5900;
     private static readonly HashSet<byte> Playerids = [];
     public static bool HasEnabled => Playerids.Any();
-    
-    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+
+    public override CustomRoles ThisRoleBase => LegacyParasite.GetBool() ? CustomRoles.Shapeshifter : CustomRoles.Impostor;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.Madmate;
     //==================================================================\\
 
     private static OptionItem ParasiteCD;
+    private static OptionItem LegacyParasite;
+    private static OptionItem ParasiteShapeshiftCD;
+    private static OptionItem ParasiteShapeshiftDur;
 
     public override void SetupCustomOption()
     {
@@ -22,6 +25,14 @@ internal class Parasite : RoleBase
         ParasiteCD = FloatOptionItem.Create(Id + 2, GeneralOption.KillCooldown, new(0f, 180f, 2.5f), 30f, TabGroup.ImpostorRoles, false)
             .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Parasite])
             .SetValueFormat(OptionFormat.Seconds);
+        LegacyParasite = BooleanOptionItem.Create(Id + 3, "LegacyParasite", false, TabGroup.ImpostorRoles, false)
+                .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Parasite]);
+        ParasiteShapeshiftCD = FloatOptionItem.Create(Id + 4, GeneralOption.ShapeshifterBase_ShapeshiftCooldown, new(1f, 180f, 1f), 15f, TabGroup.ImpostorRoles, false)
+                .SetParent(LegacyParasite)
+                .SetValueFormat(OptionFormat.Seconds);
+        ParasiteShapeshiftDur = FloatOptionItem.Create(Id + 5, GeneralOption.ShapeshifterBase_ShapeshiftDuration, new(1f, 180f, 1f), 30f, TabGroup.ImpostorRoles, false)
+                .SetParent(LegacyParasite)
+                .SetValueFormat(OptionFormat.Seconds);
     }
     public override void Init()
     {
@@ -31,10 +42,13 @@ internal class Parasite : RoleBase
     {
         Playerids.Add(playerId);
     }
-
-    public override void ApplyGameOptions(IGameOptions opt, byte playerId) => opt.SetVision(true);
+    public override void ApplyGameOptions(IGameOptions opt, byte playerId) 
+    {
+        opt.SetVision(true);
+        AURoleOptions.ShapeshifterCooldown = ParasiteShapeshiftCD.GetFloat();
+        AURoleOptions.ShapeshifterDuration = ParasiteShapeshiftDur.GetFloat();
+    }
     public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = ParasiteCD.GetFloat();
-
     public override bool CanUseKillButton(PlayerControl pc) => true;
     public override bool CanUseImpostorVentButton(PlayerControl pc) => true;
     public override bool CanUseSabotage(PlayerControl pc) => true;
