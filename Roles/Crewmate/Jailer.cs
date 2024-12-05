9d7bafe9b63a06d16c9531e71d5304fd5ef63189
ImpyIsLazy <impostoramongus71@gmail.com>
2024-12-06 01:32:25 +0800

    Update Jailer.cs

--- Roles/Crewmate/Jailer.cs
+++ Roles/Crewmate/Jailer.cs
@@ -185,8 +185,8 @@ internal class Jailer : RoleBase
                 (role.IsNE() && NECanBeExe.GetBool()) ||
                 (role.IsNK() && NKCanBeExe.GetBool()) ||
                 (role.IsNA() && NACanBeExe.GetBool()) ||
-                (role.IsCrewKiller() && CKCanBeExe.GetBool()) ||
-                role.IsImpostorTeamV3();
+                ((role.IsCrewKiller() || role.Is(CustomRoles.Narc)) && CKCanBeExe.GetBool()) ||
+                (role.IsImpostorTeamV3() && !role.Is(CustomRoles.Narc));
     }
 
     public override void OnPlayerExiled(PlayerControl player, NetworkedPlayerInfo exiled)
