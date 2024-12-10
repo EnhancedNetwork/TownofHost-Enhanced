4ca6cfc8165f95d946bdc574aaa7661c9ce8acff
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-12-10 22:03:22 +0800

    220 Alpha 8

--- main.cs
+++ main.cs
@@ -47,8 +47,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.1207.220.00070"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.2.0 Alpha 7";
+    public const string PluginVersion = "2024.1210.220.00080"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.2.0 Alpha 8";
     public const string SupportedVersionAU = "2024.10.29"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
