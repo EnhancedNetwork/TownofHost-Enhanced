88fef10a878b2672dd7a7ff67b4c1672987c1d24
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-12-12 10:58:12 +0800

    Change logging of consigliere

--- Roles/Impostor/Consigliere.cs
+++ Roles/Impostor/Consigliere.cs
@@ -8,9 +8,6 @@ internal class Consigliere : RoleBase
 {
     //===========================SETUP================================\\
     private const int Id = 3100;
-
-
-
     public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorSupport;
     //==================================================================\\
@@ -94,7 +91,7 @@ internal class Consigliere : RoleBase
         {
             DivinationCount[killer.PlayerId]--;
             DivinationTarget[killer.PlayerId].Add(target.PlayerId);
-            Logger.Info($"{killer.GetNameWithRole()}：占った 占い先→{target.GetNameWithRole()} || 残り{DivinationCount[killer.PlayerId]}回", "Consigliere");
+            Logger.Info($"{killer.GetNameWithRole()}：Checked→{target.GetNameWithRole()} || Remaining Ability: {DivinationCount[killer.PlayerId]}", "Consigliere");
             Utils.NotifyRoles(SpecifySeer: killer, SpecifyTarget: target, ForceLoop: true);
 
             SendRPC(killer.PlayerId, target.PlayerId);
