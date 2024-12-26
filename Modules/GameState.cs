69addf7ce853b9d1759ed63dafe54399c27e3a60
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-12-26 13:38:36 +0800

    Add Role convert log

--- Modules/GameState.cs
+++ Modules/GameState.cs
@@ -27,6 +27,7 @@ public class PlayerState(byte playerId)
     public TaskState taskState = new();
     public bool IsBlackOut { get; set; } = false;
     public (DateTime, byte) RealKiller = (DateTime.MinValue, byte.MaxValue);
+    public List<(DateTime, CustomRoles)> MainRoleLogs = []; 
     public PlainShipRoom LastRoom = null;
     public bool HasSpawned { get; set; } = false;
     public Dictionary<byte, string> TargetColorData = [];
@@ -96,6 +97,8 @@ public class PlayerState(byte playerId)
         {
             countTypes = CountTypes.OutOfGame;
         }
+
+        MainRoleLogs.Add((DateTime.Now, role));
 
         if (GameStates.IsInGame && preMainRole != CustomRoles.NotAssigned)
         {
