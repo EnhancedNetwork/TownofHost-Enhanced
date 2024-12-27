a6bbf15ce77c2fce949e52cc4428ebc1fea77244
ImpyIsLazy <impostoramongus71@gmail.com>
2024-12-27 23:21:52 +0800

    copycat-admired

--- Roles/Crewmate/CopyCat.cs
+++ Roles/Crewmate/CopyCat.cs
@@ -153,6 +153,7 @@ internal class CopyCat : RoleBase
                 if (target.Is(CustomRoles.Recruit)) killer.RpcSetCustomRole(CustomRoles.Recruit);
                 if (target.Is(CustomRoles.Contagious)) killer.RpcSetCustomRole(CustomRoles.Contagious);
                 if (target.Is(CustomRoles.Soulless)) killer.RpcSetCustomRole(CustomRoles.Soulless);
+                if (target.Is(CustomRoles.Admired)) killer.RpcSetCustomRole(CustomRoles.Admired);
             }
             killer.RpcGuardAndKill(killer);
             killer.Notify(string.Format(GetString("CopyCatRoleChange"), Utils.GetRoleName(role)));
