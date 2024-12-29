109df5283c390d30894004949f63580da252a5e3
ImpyIsLazy <impostoramongus71@gmail.com>
2024-12-29 22:54:10 +0800

    Update Gangster.cs

--- Roles/Impostor/Gangster.cs
+++ Roles/Impostor/Gangster.cs
@@ -75,9 +75,9 @@ internal class Gangster : RoleBase
 
         if (CanRecruit(killer.PlayerId))
         {
-            if (!killer.Is(CustomRoles.Admired) && !killer.Is(CustomRoles.Narc) && !killer.Is(CustomRoles.Recruit) && !killer.Is(CustomRoles.Charmed)
-                && !killer.Is(CustomRoles.Infected) && !killer.Is(CustomRoles.Contagious) && target.CanBeMadmate(forGangster: true)
-                 && CanBeGansterRecruit(target))
+            if (!killer.Is(CustomRoles.Admired) && !killer.Is(CustomRoles.Recruit) && !killer.Is(CustomRoles.Charmed)
+                && !killer.Is(CustomRoles.Infected) && !killer.Is(CustomRoles.Contagious) && !killer.Is(CustomRoles.Narc) && target.CanBeMadmate(forGangster: true)
+               && CanBeGansterRecruit(target))
             {
                 Logger.Info("Set converted: " + target.GetNameWithRole().RemoveHtmlTags() + " to " + CustomRoles.Madmate.ToString(), "Ganster Assign");
                 target.RpcSetCustomRole(CustomRoles.Madmate);
