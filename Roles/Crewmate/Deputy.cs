b4ec4b8a662964a0db7a45c1cd6e52657cc679ce
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-12-29 20:50:33 +0800

    Rework Deputy due to the fact that it was coded to shit

--- Roles/Crewmate/Deputy.cs
+++ Roles/Crewmate/Deputy.cs
@@ -18,6 +18,7 @@ internal class Deputy : RoleBase
     private static OptionItem HandcuffCooldown;
     private static OptionItem HandcuffMax;
     private static OptionItem DeputyHandcuffCDForTarget;
+    private static OptionItem HandcuffBrokenAfterMeeting;
 
     private static readonly Dictionary<byte, List<byte>> RoleblockedPlayers = [];
 
@@ -30,6 +31,7 @@ internal class Deputy : RoleBase
             .SetValueFormat(OptionFormat.Seconds);
         HandcuffMax = IntegerOptionItem.Create(Id + 12, "DeputyHandcuffMax", new(1, 30, 1), 15, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Deputy])
             .SetValueFormat(OptionFormat.Times);
+        HandcuffBrokenAfterMeeting = BooleanOptionItem.Create(Id + 16, "HandcuffBrokenAfterMeeting", true, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Deputy]);
     }
     public override void Init()
     {
@@ -50,13 +52,24 @@ internal class Deputy : RoleBase
 
         Logger.Info($"{killer.GetNameWithRole().RemoveHtmlTags()} : Limit {AbilityLimit}", "Deputy");
 
-        if (target != _Player)
+        if (target.PlayerId != _Player.PlayerId)
         {
-            RoleblockedPlayers[killer.PlayerId].Add(target.PlayerId);
-            AbilityLimit--;
-            SendSkillRPC();
+            if (!RoleblockedPlayers[killer.PlayerId].Contains(target.PlayerId))
+            {
+                RoleblockedPlayers[killer.PlayerId].Add(target.PlayerId);
+                AbilityLimit--;
+                SendSkillRPC();
+
+                killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Deputy), GetString("DeputyHandcuffedPlayer")));
+                killer.SetKillCooldown();
+            }
+            else
+            {
+                // Target already have a handcuff
+                killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Deputy), GetString("DeputyInvalidTarget")));
+            }
         }
-        killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Deputy), GetString("DeputyInvalidTarget")));
+        
         return false;
     }
     public override bool CheckMurderOnOthersTarget(PlayerControl killer, PlayerControl target)
@@ -64,12 +77,24 @@ internal class Deputy : RoleBase
         if (!IsRoleblocked(killer.PlayerId) && killer.GetCustomRole() is not CustomRoles.SerialKiller or CustomRoles.Pursuer or CustomRoles.Deputy or CustomRoles.Deceiver or CustomRoles.Poisoner) return false; // I was told these roles should be roleblock immune
         if (killer == null) return false;
         killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Deputy), GetString("HandcuffedByDeputy")));
-        killer.SetKillCooldownV3(DeputyHandcuffCDForTarget.GetFloat());
-        if (!DisableShieldAnimations.GetBool()) killer.RpcGuardAndKill(killer);
+        killer.SetKillCooldownV3(DeputyHandcuffCDForTarget.GetFloat(), forceAnime: !DisableShieldAnimations.GetBool());
+        killer.ResetKillCooldown();
+
+        RemoveRoleblock(killer.PlayerId);
         Logger.Info($"{killer.GetRealName()} fail ability because roleblocked", "Deputy");
-        return false;
+        return true;
     }
-    public static bool IsRoleblocked(byte target)
+    public override void AfterMeetingTasks()
+    {
+        if (HandcuffBrokenAfterMeeting.GetBool())
+        {
+            foreach (var player in RoleblockedPlayers.Keys)
+            {
+                RoleblockedPlayers[player].Clear();
+            }
+        }
+    }
+    private static bool IsRoleblocked(byte target)
     {
         if (RoleblockedPlayers.Count < 1) return false;
         foreach (var player in RoleblockedPlayers.Keys)
@@ -77,6 +102,16 @@ internal class Deputy : RoleBase
             if (RoleblockedPlayers[player].Contains(target)) return true;
         }
         return false;
+    }
+
+    private static void RemoveRoleblock(byte target)
+    {
+        if (RoleblockedPlayers.Count < 1) return;
+        foreach (var player in RoleblockedPlayers.Keys)
+        {
+            if (RoleblockedPlayers[player].Contains(target)) 
+                RoleblockedPlayers[player].Remove(target);
+        }
     }
     public override string GetProgressText(byte PlayerId, bool comms) => Utils.ColorString(AbilityLimit >= 1 ? Utils.GetRoleColor(CustomRoles.Deputy) : Color.gray, $"({AbilityLimit})");
     public override void SetAbilityButtonText(HudManager hud, byte id)
