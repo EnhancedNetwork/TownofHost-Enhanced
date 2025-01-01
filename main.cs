f4cb531e925a33eeb20fb1630a8b5a2557529a07
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-01-02 00:51:17 +0800

    220 Beta 1

--- main.cs
+++ main.cs
@@ -46,13 +46,13 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2024.1230.220.00100"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.2.0 Alpha 10";
+    public const string PluginVersion = "2025.0102.220.10000"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.2.0 Beta 1";
     public const string SupportedVersionAU = "2024.10.29"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
     public static readonly bool devRelease = false; // Latest: V2.2.0 Alpha 4 Hotfix 1
-    public static readonly bool canaryRelease = true; // Latest: V2.1.0 Beta 3
+    public static readonly bool canaryRelease = true; // Latest: V2.2.0 Beta 1
     public static readonly bool fullRelease = false; // Latest: V2.1.1
 
     public static bool hasAccess = true;
