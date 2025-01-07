1b90ed469811d0922d5e27341566e63b8f0d4dd9
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-08 05:14:50 +0800

    Update CustomRoleManager.cs

--- Roles/Core/CustomRoleManager.cs
+++ Roles/Core/CustomRoleManager.cs
@@ -166,6 +166,9 @@ public static class CustomRoleManager
                     case CustomRoles.Mare:
                         Mare.ApplyGameOptions(player.PlayerId);
                         break;
+                    case CustomRoles.Narc:
+                        Narc.ApplyGameOptions(opt, player);
+                        break;
                 }
             }
 
@@ -262,6 +265,11 @@ public static class CustomRoleManager
                         if (!Swift.OnCheckMurder(killer, target))
                             cancelbutkill = true;
                         break;
+
+                    case CustomRoles.Narc:
+                        if (Narc.CancelMurder(killer,target))
+                            canceled = true;
+                    break;
                 }
             }
 
