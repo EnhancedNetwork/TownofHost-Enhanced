e479abf26d566431813b45f7fa79f618d0c69a03
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-08 05:25:50 +0800

    Update Captain.cs

--- Roles/Crewmate/Captain.cs
+++ Roles/Crewmate/Captain.cs
@@ -68,7 +68,7 @@ internal class Captain : RoleBase
         if (pc.GetPlayerTaskState().CompletedTasksCount >= OptionTaskRequiredToReveal.GetInt()) NotifyRoles(SpecifyTarget: pc, ForceLoop: true);
         if (pc.GetPlayerTaskState().CompletedTasksCount < OptionTaskRequiredToSlow.GetInt()) return true;
         var allTargets = Main.AllAlivePlayerControls.Where(x => (x != null) && (!OriginalSpeed.ContainsKey(x.PlayerId)) &&
-                                                           (x.GetCustomRole().IsImpostorTeamV3() ||
+                                                           (x.IsNonNarcImpV3() ||
                                                            (CaptainCanTargetNB.GetBool() && x.GetCustomRole().IsNB()) ||
                                                            (CaptainCanTargetNE.GetBool() && x.GetCustomRole().IsNE()) ||
                                                            (CaptainCanTargetNC.GetBool() && x.GetCustomRole().IsNC()) ||
@@ -157,7 +157,7 @@ internal class Captain : RoleBase
     {
         if (target.Is(CustomRoles.Captain) && OptionCrewCanFindCaptain.GetBool() &&
                 (target.GetPlayerTaskState().CompletedTasksCount >= OptionTaskRequiredToReveal.GetInt()) &&
-                ((seer.Is(Custom_Team.Crewmate) && !seer.Is(CustomRoles.Madmate)) || (seer.Is(CustomRoles.Madmate) && OptionMadmateCanFindCaptain.GetBool())))
+                ((seer.IsNarcCrew() && !seer.Is(CustomRoles.Madmate)) || (seer.Is(CustomRoles.Madmate) && OptionMadmateCanFindCaptain.GetBool())))
         {
             return ColorString(GetRoleColor(CustomRoles.Captain), " ☆");
         }
