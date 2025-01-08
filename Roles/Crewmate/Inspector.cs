582a57a48aadde7b5d73ffde7867bc96bfeba0cf
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-09 00:17:30 +0800

    Update Inspector.cs

--- Roles/Crewmate/Inspector.cs
+++ Roles/Crewmate/Inspector.cs
@@ -251,15 +251,15 @@ internal class Inspector : RoleBase
                         )
                         ||
                         (
-                        (target1.GetCustomRole().IsImpostorTeamV2() || target1.IsAnySubRole(role => role.IsImpostorTeamV2())) && !target1.Is(CustomRoles.Admired) && !target1.Is(CustomRoles.Narc))
+                        (target1.GetCustomRole().IsImpostorTeamV2() || target1.IsAnySubRole(role => role.IsImpostorTeamV2())) && !target1.Is(CustomRoles.Admired) && !target1.Is(CustomRoles.Narc)
                         &&
-                        (target2.GetCustomRole().IsImpostorTeamV2() || target2.IsAnySubRole(role => role.IsImpostorTeamV2())) && !target2.Is(CustomRoles.Admired) && !target2.Is(CustomRoles.Narc))
+                        (target2.GetCustomRole().IsImpostorTeamV2() || target2.IsAnySubRole(role => role.IsImpostorTeamV2())) && !target2.Is(CustomRoles.Admired) && !target2.Is(CustomRoles.Narc)
                         )
                         ||
                         (
-                        (target1.GetCustomRole().IsNeutralTeamV2() || target1.IsAnySubRole(role => role.IsNeutralTeamV2())) && !target1.Is(CustomRoles.Admired)  && !target2.Is(CustomRoles.Narc)
+                        (target1.GetCustomRole().IsNeutralTeamV2() || target1.IsAnySubRole(role => role.IsNeutralTeamV2())) && !target1.Is(CustomRoles.Admired) && !target1.Is(CustomRoles.Narc)
                         &&
-                        (target2.GetCustomRole().IsNeutralTeamV2() || target2.IsAnySubRole(role => role.IsNeutralTeamV2())) && !target2.Is(CustomRoles.Admired)  && !target2.Is(CustomRoles.Narc)
+                        (target2.GetCustomRole().IsNeutralTeamV2() || target2.IsAnySubRole(role => role.IsNeutralTeamV2())) && !target2.Is(CustomRoles.Admired) && !target2.Is(CustomRoles.Narc)
                         )
                     )
                     {
