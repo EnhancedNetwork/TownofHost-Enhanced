622e5ee0ab2a7a7aaa33f7eb55bee0bfc9eb8432
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-08 22:28:02 +0800

    Update Refugee.cs

--- Roles/Impostor/Refugee.cs
+++ Roles/Impostor/Refugee.cs
@@ -12,17 +12,7 @@ internal class Refugee : RoleBase
     public override Custom_RoleType ThisRoleType => Custom_RoleType.Madmate;
     //==================================================================\\
 
-    private static OptionItem RefugeeKillCD;
-
-    public override void SetupCustomOption()
-    {
-        RefugeeKillCD = FloatOptionItem.Create(Id, "RefugeeKillCD", new(0f, 180f, 2.5f), 22.5f, TabGroup.ImpostorRoles, false)
-            .SetHeader(true)
-            .SetValueFormat(OptionFormat.Seconds)
-            .SetGameMode(CustomGameMode.Standard);
-    }
     public override void ApplyGameOptions(IGameOptions opt, byte playerId) => opt.SetVision(true);
-    public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = RefugeeKillCD.GetFloat();
 
     public override bool CanUseKillButton(PlayerControl pc) => true;
     public override bool CanUseImpostorVentButton(PlayerControl pc) => true;
