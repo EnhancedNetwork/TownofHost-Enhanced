40cd0f61292423fbbbdf4327cab0c3a60cb1a3e3
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-01-16 12:49:07 +0800

    220 alpha 12

--- main.cs
+++ main.cs
@@ -46,7 +46,7 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2025.0109.220.12000"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginVersion = "2025.0116.220.12000"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "2.2.0 Alpha 12";
     public const string SupportedVersionAU = "2024.10.29"; // Changed becasue Dark theme works at this version.
 
