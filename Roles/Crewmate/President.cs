5c18e71601ce6a4b8b1f2183af9ccc767527168d
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-16 21:57:16 -0700

    Update President.cs

--- Roles/Crewmate/President.cs
+++ Roles/Crewmate/President.cs
@@ -1,4 +1,4 @@
-﻿using Hazel;
+using Hazel;
 using TOHE.Modules.ChatManager;
 using UnityEngine;
 using static TOHE.Translator;
@@ -22,7 +22,6 @@ internal class President : RoleBase
     private static OptionItem ImpsSeePresident;
     private static OptionItem CovenSeePresident;
 
-    private static readonly Dictionary<byte, int> EndLimit = [];
     private static readonly Dictionary<byte, int> RevealLimit = [];
     private static readonly Dictionary<byte, bool> CheckPresidentReveal = [];
 
@@ -41,24 +40,21 @@ internal class President : RoleBase
     public override void Init()
     {
         CheckPresidentReveal.Clear();
-        EndLimit.Clear();
         RevealLimit.Clear();
     }
     public override void Add(byte playerId)
     {
         CheckPresidentReveal.Add(playerId, false);
-        EndLimit.Add(playerId, PresidentAbilityUses.GetInt());
         RevealLimit.Add(playerId, 1);
+        playerId.SetAbilityUseLimit(PresidentAbilityUses.GetInt());
     }
     public override void Remove(byte playerId)
     {
         CheckPresidentReveal.Remove(playerId);
-        EndLimit.Remove(playerId);
         RevealLimit.Remove(playerId);
     }
 
     public static bool CheckReveal(byte targetId) => CheckPresidentReveal.TryGetValue(targetId, out var canBeReveal) && canBeReveal;
-    public override string GetProgressText(byte PlayerId, bool comms) => Utils.ColorString(EndLimit[PlayerId] > 0 ? Utils.GetRoleColor(CustomRoles.President) : Color.gray, EndLimit.TryGetValue(PlayerId, out var endLimit) ? $"({endLimit})" : "Invalid");
 
     public static void TryHideMsgForPresident()
     {
@@ -118,13 +114,12 @@ internal class President : RoleBase
             }
             else if (pc.AmOwner) Utils.SendMessage(originMsg, 255, pc.GetRealName());
 
-            if (EndLimit[pc.PlayerId] < 1)
+            if (pc.GetAbilityUseLimit() < 1)
             {
                 Utils.SendMessage(GetString("PresidentEndMax"), pc.PlayerId);
                 return true;
             }
-
-            EndLimit[pc.PlayerId]--;
+            pc.RpcRemoveAbilityUse();
 
             foreach (var pva in MeetingHud.Instance.playerStates)
             {
@@ -217,8 +212,7 @@ internal class President : RoleBase
         if (!isEnd)
         {
             bool revealed = reader.ReadBoolean();
-            if (CheckPresidentReveal.ContainsKey(PlayerId)) CheckPresidentReveal[PlayerId] = revealed;
-            else CheckPresidentReveal.Add(PlayerId, false);
+            CheckPresidentReveal[PlayerId] = revealed;
             return;
         }
         EndMsg(pc, $"/finish");
