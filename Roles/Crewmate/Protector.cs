56441b799ca63d4dd597934250a7fc155251dd83
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-17 01:04:35 -0700

    Update Protector.cs

--- Roles/Crewmate/Protector.cs
+++ Roles/Crewmate/Protector.cs
@@ -39,7 +39,7 @@ internal class Protector : RoleBase
     }
     public override void Add(byte playerId)
     {
-        AbilityLimit = MaxShields.GetInt();
+        playerId.SetAbilityUseLimit(MaxShields.GetInt());
         TimeStamp = 0;
 
         if (!Main.ResetCamPlayerList.Contains(playerId))
@@ -61,18 +61,6 @@ internal class Protector : RoleBase
     }
     private bool ProtectorInProtect(byte playerId) => TimeStamp > Utils.GetTimeStamp();
 
-    public override bool OnTaskComplete(PlayerControl player, int completedTaskCount, int totalTaskCount)
-    {
-        if (player.IsAlive())
-            if (AbilityLimit >= 1)
-            {
-                AbilityLimit--;
-                TimeStamp = Utils.GetTimeStamp() + (long)ShieldDuration.GetFloat();
-                SendRPC(player.PlayerId);
-                player.Notify(GetString("ProtectorInProtect"));
-            }
-        return true;
-    }
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
     {
         if (ProtectorInProtect(target.PlayerId))
