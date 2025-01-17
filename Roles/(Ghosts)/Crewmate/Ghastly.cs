84b691f28cb161f0f68815b6f34b78e1d3ae022d
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-17 11:12:45 -0700

    Update Ghastly.cs

--- Roles/(Ghosts)/Crewmate/Ghastly.cs
+++ Roles/(Ghosts)/Crewmate/Ghastly.cs
@@ -3,7 +3,6 @@ using Hazel;
 using InnerNet;
 using TOHE.Roles.Core;
 using TOHE.Roles.Double;
-using UnityEngine;
 using static TOHE.Options;
 using static TOHE.Translator;
 using static TOHE.Utils;
