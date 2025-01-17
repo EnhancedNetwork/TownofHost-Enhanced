249d728617041fcd8aa9ca3f37ae29e078a5de2a
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-18 01:22:31 +0800

    font size

--- Patches/PlayerControlPatch.cs
+++ Patches/PlayerControlPatch.cs
@@ -1323,7 +1323,7 @@ class FixedUpdateInNormalGamePatch
                     RoleText.enabled = true; //have to make it return true otherwise modded Overseer won't be able to reveal Trickster's role,same for Illusionist's targets
                     BlankRT = Overseer.GetRandomRole(PlayerControl.LocalPlayer.PlayerId); // random role for revealed trickster
                     BlankRT += TaskState.GetTaskState(); // random task count for revealed trickster
-                    RoleText.text = $"<size=1.4>{BlankRT}</size>";
+                    RoleText.text = $"<size=1.3>{BlankRT}</size>";
                 }
                 if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && __instance.Is(CustomRoles.Narc) && !PlayerControl.LocalPlayer.Is(CustomRoles.Madmate))
                 {
@@ -1331,14 +1331,14 @@ class FixedUpdateInNormalGamePatch
                     BlankRT = Utils.ColorString(Utils.GetRoleColor(CustomRoles.Sheriff), GetString(CustomRoles.Sheriff.ToString())); //Sheriff
                     if (Sheriff.ShowShotLimit.GetBool()) 
                         BlankRT += Utils.ColorString(Utils.GetRoleColor(CustomRoles.Sheriff).ShadeColor(0.25f), $"({Sheriff.ShotLimitOpt.GetInt()})"); // Sheriff progress text
-                    RoleText.text = $"<size=1.4>{BlankRT}</size>";
+                    RoleText.text = $"<size=1.3>{BlankRT}</size>";
                 }
                 if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && Illusionist.IsCovIllusioned(__instance.PlayerId))
                 {
                     RoleText.enabled = true;
                     BlankRT = Overseer.GetRandomRole(PlayerControl.LocalPlayer.PlayerId);
                     BlankRT += TaskState.GetTaskState();
-                    RoleText.text = $"<size=1.4>{BlankRT}</size>";
+                    RoleText.text = $"<size=1.3>{BlankRT}</size>";
                 }
                 if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && Illusionist.IsNonCovIllusioned(__instance.PlayerId))
                 {
@@ -1349,7 +1349,7 @@ class FixedUpdateInNormalGamePatch
                     {
                         BlankRT += randomRole.GetStaticRoleClass().GetProgressText(PlayerControl.LocalPlayer.PlayerId, false);
                     }
-                    RoleText.text = $"<size=1.4>{BlankRT}</size>";
+                    RoleText.text = $"<size=1.3>{BlankRT}</size>";
                 }
 
 
