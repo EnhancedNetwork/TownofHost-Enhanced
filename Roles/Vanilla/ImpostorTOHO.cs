b34f559e6bfd1ce5b6f149365d27cd100e932fa6
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-18 11:43:12 -0700

    Update and rename ImpostorTOHE.cs to ImpostorTOHO.cs

--- Roles/Vanilla/ImpostorTOHO.cs
+++ Roles/Vanilla/ImpostorTOHO.cs
@@ -4,7 +4,7 @@ namespace TOHE.Roles.Vanilla;
 internal class ImpostorTOHE : RoleBase
 {
     //===========================SETUP================================\\
-    public override CustomRoles Role => CustomRoles.ImpostorTOHE;
+    public override CustomRoles Role => CustomRoles.ImpostorTOHO;
     private const int Id = 300;
     public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorVanilla;
