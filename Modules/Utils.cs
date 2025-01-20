5fb82ab99cee996cb68311e2f2016779db9c8485
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-20 23:43:57 +0800

    Update Utils.cs

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -2173,7 +2173,7 @@ public static class Utils
 
                         string BlankRT = string.Empty;
 
-                        if (seer.IsAlive() && Overseer.IsRevealedPlayer(seer, target) && target.Is(CustomRoles.Trickster))
+                        if (seer.IsAlive() && Overseer.IsRevealedPlayer(seer, target) && target.Is(CustomRoles.Trickster) && (!target.Is(CustomRoles.Narc) || seer.Is(CustomRoles.Madmate)))
                         {
                             BlankRT = Overseer.GetRandomRole(seer.PlayerId); // Random trickster role
                             BlankRT += TaskState.GetTaskState(); // Random task count for revealed trickster
@@ -2194,6 +2194,13 @@ public static class Utils
                             {
                                 BlankRT += randomRole.GetStaticRoleClass().GetProgressText(target.PlayerId, false);
                             }
+                            TargetRoleText = $"<size={fontSize}>{BlankRT}</size>\r\n";
+                        }
+                        if (seer.IsAlive() && Overseer.IsRevealedPlayer(seer, target) && target.Is(CustomRoles.Narc) && !seer.Is(CustomRoles.Madmate))
+                        {
+                            BlankRT = ColorString(GetRoleColor(CustomRoles.Sheriff), GetString(CustomRoles.Sheriff.ToString())); //Sheriff
+                            if (Sheriff.ShowShotLimit.GetBool()) 
+                                BlankRT += $" {ColorString(GetRoleColor(CustomRoles.Sheriff).ShadeColor(0.25f), $"({Sheriff.ShotLimitOpt.GetInt()})")}"; // Sheriff progress text
                             TargetRoleText = $"<size={fontSize}>{BlankRT}</size>\r\n";
                         }
 
