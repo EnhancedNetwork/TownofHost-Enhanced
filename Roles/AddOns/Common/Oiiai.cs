c7b24b31b2a4195ed90a7f78758a2d244c2de7a6
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-21 22:25:13 +0800

    Update Oiiai.cs

--- Roles/AddOns/Common/Oiiai.cs
+++ Roles/AddOns/Common/Oiiai.cs
@@ -113,8 +113,10 @@ public class Oiiai : IAddon
         }
         else if (killer.GetCustomRole().IsMadmate())
         {
-            killer.RpcChangeRoleBasis(CustomRoles.Convict);
-            killer.RpcSetCustomRole(CustomRoles.Convict);
+            killer.RpcChangeRoleBasis(CustomRoles.Amnesiac);
+            killer.RpcSetCustomRole(CustomRoles.Amnesiac);
+            killer.RpcSetCustomRole(CustomRoles.Madmate, false);
+            killer.AddInSwitchAddons(killer, CustomRoles.Madmate);
             Logger.Info($"Oiiai {killer.GetNameWithRole().RemoveHtmlTags()} with Madmates assign.", "Oiiai");
         }
         else if (killer.Is(CustomRoles.Sidekick))
