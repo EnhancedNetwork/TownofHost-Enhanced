7913c04e735552a378206942497c9a1215ddd76a
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-21 07:52:54 -0700

    Update Infectious.cs

--- Roles/Neutral/Infectious.cs
+++ Roles/Neutral/Infectious.cs
@@ -68,7 +68,7 @@ internal class Infectious : RoleBase
         if (CanBeBitten(target))
         {
             BiteLimit--;
-            target.RpcSetCustomRole(CustomRoles.Infected);
+            target.RpcSetCustomRole(CustomRoles.Infected, false);
 
             Utils.NotifyRoles(SpecifySeer: killer, SpecifyTarget: target, ForceLoop: true);
             Utils.NotifyRoles(SpecifySeer: target, SpecifyTarget: killer, ForceLoop: true);
