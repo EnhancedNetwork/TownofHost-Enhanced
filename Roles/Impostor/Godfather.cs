13fedb09e90927a73bcce77aa7f03bc0c448166d
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-21 07:50:36 -0700

    Update Godfather.cs

--- Roles/Impostor/Godfather.cs
+++ Roles/Impostor/Godfather.cs
@@ -66,7 +66,7 @@ internal class Godfather : RoleBase
             }
             else
             {
-                killer.RpcSetCustomRole(ChangeAddon);
+                killer.RpcSetCustomRole(ChangeAddon, false);
             }
 
             killer.RpcGuardAndKill();
