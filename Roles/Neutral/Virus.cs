25da17aaa6d6d0dc8d91e57b4c611dd0bdb703ce
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-21 11:38:51 -0700

    Update Virus.cs

--- Roles/Neutral/Virus.cs
+++ Roles/Neutral/Virus.cs
@@ -2,6 +2,7 @@
 using AmongUs.GameOptions;
 using TOHE.Roles.AddOns.Crewmate;
 using TOHE.Roles.Core;
+using TOHE.Modules;
 using static TOHE.Options;
 using static TOHE.Translator;
 using static TOHE.MeetingHudStartPatch;
