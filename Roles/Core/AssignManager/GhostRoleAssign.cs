2c43981ee2b2ea45d876ca50799c86e0b272cf76
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-21 08:36:07 -0700

    Update GhostRoleAssign.cs

--- Roles/Core/AssignManager/GhostRoleAssign.cs
+++ Roles/Core/AssignManager/GhostRoleAssign.cs
@@ -56,6 +56,7 @@ public static class GhostRoleAssign
             or CustomRoles.Retributionist
             or CustomRoles.NiceMini
             or CustomRoles.Romantic
+            or CustomRoles.Jester
             or CustomRoles.Follower
             or CustomRoles.Specter
             or CustomRoles.Sunnyboy
@@ -65,6 +66,8 @@ public static class GhostRoleAssign
             or CustomRoles.Lawyer
             or CustomRoles.Provocateur
             or CustomRoles.Virus
+            or CustomRoles.Jackal
+            or CustomRoles.Sidekick
             or CustomRoles.PlagueDoctor) return;
 
         var IsNeutralAllowed = !player.IsAnySubRole(x => x.IsConverted()) || Options.ConvertedCanBecomeGhost.GetBool();
