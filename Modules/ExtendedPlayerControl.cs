60d8bda2ef9887d016d40bdde4a7ae229236e19b
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-22 23:56:09 +0800

    Update ExtendedPlayerControl.cs

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -19,7 +19,7 @@ namespace TOHE;
 
 static class ExtendedPlayerControl
 {
-    public static void RpcSetCustomRole(this PlayerControl player, CustomRoles role, bool checkAddons = true)
+    public static void RpcSetCustomRole(this PlayerControl player, CustomRoles role,  bool checkAddons = true/*check role-addon*/, bool checkAAconflict = true/*check addon-addon*/)
     {
         if (role < CustomRoles.NotAssigned)
         {
@@ -31,9 +31,14 @@ static class ExtendedPlayerControl
         else if (role >= CustomRoles.NotAssigned)   //500:NoSubRole 501~:SubRole 
         {
             if (Cleanser.CantGetAddon() && player.Is(CustomRoles.Cleansed)) return;
+
             if (role == CustomRoles.Cleansed) Main.PlayerStates[player.PlayerId].SetSubRole(role, pc: player);
+            else if (role.IsBetrayalAddonV2()) player.AddInSwitchAddons(player, role);
             else Main.PlayerStates[player.PlayerId].SetSubRole(role);
-            if (checkAddons) player.RemoveIncompatibleAddOns();
+
+            if (role.IsAddonAssignedMidGame()) checkAAconflict = false;
+
+            if (checkAAconflict) player.RemoveIncompatibleAddOns();
         }
         if (AmongUsClient.Instance.AmHost)
         {
