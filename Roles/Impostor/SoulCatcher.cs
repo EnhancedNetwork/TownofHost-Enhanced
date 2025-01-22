68e40c196a6796a2a0c7f4c67caedd74875dd6a9
Bri-11 <4brianjr11@gmail.com>
2025-01-21 20:13:00 -0500

    Update SoulCatcher.cs

--- Roles/Impostor/SoulCatcher.cs
+++ Roles/Impostor/SoulCatcher.cs
@@ -1,6 +1,9 @@
 using AmongUs.GameOptions;
 using TOHE.Modules;
 using UnityEngine;
+using static TOHE.Options;
+using static TOHE.Translator;
+using static TOHE.Utils;
 
 namespace TOHE.Roles.Impostor;
 
@@ -13,27 +16,21 @@ internal class SoulCatcher : RoleBase
     public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorConcealing;
     //==================================================================\\
 
-    private static OptionItem ShapeSoulCatcherShapeshiftDuration;
     private static OptionItem SoulCatcherShapeshiftCooldown;
 
     public override void SetupCustomOption()
     {
-        Options.SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.SoulCatcher);
-        ShapeSoulCatcherShapeshiftDuration = FloatOptionItem.Create(Id + 2, GeneralOption.ShapeshifterBase_ShapeshiftDuration, new(2.5f, 180f, 2.5f), 300, TabGroup.ImpostorRoles, false)
-            .SetParent(Options.CustomRoleSpawnChances[CustomRoles.SoulCatcher])
-            .SetValueFormat(OptionFormat.Seconds);
+        SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.SoulCatcher);
         SoulCatcherShapeshiftCooldown = FloatOptionItem.Create(Id + 3, GeneralOption.ShapeshifterBase_ShapeshiftCooldown, new(1f, 180f, 1f), 15f, TabGroup.ImpostorRoles, false)
-            .SetParent(Options.CustomRoleSpawnChances[CustomRoles.SoulCatcher])
+            .SetParent(CustomRoleSpawnChances[CustomRoles.SoulCatcher])
             .SetValueFormat(OptionFormat.Seconds);
     }
     public override void ApplyGameOptions(IGameOptions opt, byte playerId)
     {
-        AURoleOptions.ShapeshifterDuration = ShapeSoulCatcherShapeshiftDuration.GetFloat();
         AURoleOptions.ShapeshifterCooldown = SoulCatcherShapeshiftCooldown.GetFloat();
-        AURoleOptions.ShapeshifterLeaveSkin = false;
     }
 
-    public override void SetAbilityButtonText(HudManager hud, byte id) => hud.AbilityButton.OverrideText(Translator.GetString("SoulCatcherButtonText"));
+    public override void SetAbilityButtonText(HudManager hud, byte id) => hud.AbilityButton.OverrideText(GetString("SoulCatcherButtonText"));
     public override Sprite GetAbilityButtonSprite(PlayerControl player, bool shapeshifting) => CustomButton.Get("Teleport");
 
     public override bool OnCheckShapeshift(PlayerControl shapeshifter, PlayerControl target, ref bool resetCooldown, ref bool shouldAnimate)
@@ -51,7 +48,7 @@ internal class SoulCatcher : RoleBase
         }
         else
         {
-            shapeshifter.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.SoulCatcher), Translator.GetString("ErrorTeleport")));
+            shapeshifter.Notify(ColorString(GetRoleColor(CustomRoles.SoulCatcher), GetString("ErrorTeleport")));
         }
         return false;
     }
