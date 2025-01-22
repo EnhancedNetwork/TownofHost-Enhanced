5be041508baccde71af9393bdec96f01dd2fceb9
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-22 16:20:42 -0700

    Update Illusionist.cs

--- Roles/Coven/Illusionist.cs
+++ Roles/Coven/Illusionist.cs
@@ -84,6 +84,11 @@ internal class Illusionist : CovenManager
     }
     private void SetIllusioned(PlayerControl killer, PlayerControl target)
     {
+        if (target.Is(CustomRoles.Stubborn))
+        {
+            killer.Notify(GetString("StubbornImmune"));
+            return;
+        }
         IllusionedPlayers[killer.PlayerId].Add(target.PlayerId);
         killer.RpcRemoveAbilityUse();
         SendRPC(killer, target);
