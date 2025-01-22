73fc40ec6e5ba6a7e7c1c03927c50b7a7b6f8a2b
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-01-22 17:21:24 +0800

    Test Reshaper

---
+++ TestReshaper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOHE
+{
+    public static class TestReshaper
+    {
+        public static void DoSomething()
+        {
+            int askdj;
+              bool askdjal
+                 ;
+        
+                int a = 10;
+           /*/asdasdsad/*/ int b = (int)20;
+             int c;
+            c = a + b;
+                    Logger.Info(  " asdThe resultas is: " + c, "asdkj")
+                ;
+            for (int i = 0;
+                i < 10; i++
+                )
+            {
+   
+                Logger.Info("i is: " + i, "asdkj");
+            }
+            List<int> list = new List<int>();
+              list.Add(1);
+             list.Add(2);
+                       list.Add(3);
+            foreach (var item in list) { Logger.Info("Item: " + item, "asdkj"); }
+        } } }
