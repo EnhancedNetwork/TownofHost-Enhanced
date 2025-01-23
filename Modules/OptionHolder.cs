23c7f4fc1cc95ea5aac4b4485629e879cb197068
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-23 08:25:01 -0700

    Update OptionHolder.cs

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -241,6 +241,7 @@ public static class Options
     public static OptionItem RemovePetsAtDeadPlayers;
 
     public static OptionItem CheatResponses;
+    public static OptionItem CrossLanguageGetRole;
     public static OptionItem NewHideMsg;
 
     public static OptionItem AutoDisplayKillLog;
@@ -1240,6 +1241,9 @@ public static class Options
             .HideInCandR();
 
         CheatResponses = StringOptionItem.Create(60250, "CheatResponses", CheatResponsesName, 0, TabGroup.SystemSettings, false)
+            .SetHeader(true);
+
+        CrossLanguageGetRole = BooleanOptionItem.Create(60260, "CrossLanguageGetRole", false, TabGroup.SystemSettings, false)
             .SetHeader(true);
 
         AutoDisplayKillLog = BooleanOptionItem.Create(60270, "AutoDisplayKillLog", true, TabGroup.SystemSettings, false)
