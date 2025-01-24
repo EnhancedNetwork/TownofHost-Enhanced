58f0939d8daba5308c03ace5fcd782eccf301f0f
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-24 09:12:04 -0700

    Update Utils.cs

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -2001,6 +2001,8 @@ public static class Utils
             // Do nothing when the seer is not present in the game
             if (seer == null) continue;
 
+            Main.LowLoadUpdateName[seer.PlayerId] = true;
+
             // Only non-modded players or player left
             if (seer.IsModded() || seer.PlayerId == OnPlayerLeftPatch.LeftPlayerId || seer.Data.Disconnected) continue;
 
@@ -2164,6 +2166,9 @@ public static class Utils
 
                     if (seer != target && seer != DollMaster.DollMasterTarget)
                         target = DollMaster.SwapPlayerInfo(realTarget); // If a player is possessed by the Dollmaster swap each other's controllers.
+
+                    Main.LowLoadUpdateName[target.PlayerId] = true;
+                    Main.LowLoadUpdateName[realTarget.PlayerId] = true;
 
                     //logger.Info("NotifyRoles-Loop2-" + target.GetNameWithRole() + ":START");
 
