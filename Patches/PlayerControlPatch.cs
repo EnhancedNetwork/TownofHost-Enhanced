3b03f61ed01d644d8743a50439886e68827995b6
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-26 09:51:56 +0800

    Update PlayerControlPatch.cs

--- Patches/PlayerControlPatch.cs
+++ Patches/PlayerControlPatch.cs
@@ -1401,7 +1401,7 @@ class FixedUpdateInNormalGamePatch
 
                     string BlankRT = string.Empty;
 
-                    if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && __instance.Is(CustomRoles.Trickster))
+                    if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && __instance.Is(CustomRoles.Trickster) && (!__instance.Is(CustomRoles.Narc) || PlayerControl.LocalPlayer.Is(CustomRoles.Madmate)))
                     {
                         RoleText.enabled = true; //have to make it return true otherwise modded Overseer won't be able to reveal Trickster's role,same for Illusionist's targets
                         BlankRT = Overseer.GetRandomRole(PlayerControl.LocalPlayer.PlayerId); // random role for revealed trickster
@@ -1426,7 +1426,13 @@ class FixedUpdateInNormalGamePatch
                         }
                         RoleText.text = $"<size=1.3>{BlankRT}</size>";
                     }
-
+                    if (!PlayerControl.LocalPlayer.Data.IsDead && Overseer.IsRevealedPlayer(PlayerControl.LocalPlayer, __instance) && __instance.Is(CustomRoles.Narc) && !PlayerControl.LocalPlayer.Is(CustomRoles.Madmate))
+                    {
+                        BlankRT = Utils.ColorString(Utils.GetRoleColor(CustomRoles.Sheriff), GetString(CustomRoles.Sheriff.ToString())); //Sheriff
+                        if (Sheriff.ShowShotLimit.GetBool()) 
+                            BlankRT += $" {Utils.ColorString(Utils.GetRoleColor(CustomRoles.Sheriff).ShadeColor(0.25f), $"({Sheriff.ShotLimitOpt.GetInt()})")}"; // Sheriff progress text
+                        RoleText.text = $"<size=1.3>{BlankRT}</size>";
+                    }
 
                     if (!AmongUsClient.Instance.IsGameStarted && AmongUsClient.Instance.NetworkMode != NetworkModes.FreePlay)
                     {
@@ -1507,6 +1513,13 @@ class FixedUpdateInNormalGamePatch
 
                     if (target.Is(CustomRoles.Cyber) && Cyber.CyberKnown.GetBool())
                         Mark.Append(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Cyber), "★"));
+
+                    if (target.Is(CustomRoles.Narc) 
+                        && seer.GetCustomRole() is CustomRoles.Sheriff or CustomRoles.ChiefOfPolice)
+                        Mark.Append(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Narc), "★"));
+
+                    if (target.Is(CustomRoles.Sheriff) && seer.Is(CustomRoles.Narc))
+                        Mark.Append(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Sheriff), "★"));
 
                     if (target.Is(CustomRoles.Lovers) && seer.Is(CustomRoles.Lovers))
                     {
