baae5ec1c396c0510a8e93b03d7f9d3cdbf5c10e
ImpyIsLazy <impostoramongus71@gmail.com>
2025-01-30 12:43:56 +0800

    fix

--- Modules/GameState.cs
+++ Modules/GameState.cs
@@ -163,12 +163,11 @@ public class PlayerState(byte playerId)
             }
         }
 
-        if (role.IsConverted())
+        if (role.IsBetrayalAddonV2())
         {
-            SubRoles.RemoveAll(AddON => AddON != role && AddON.IsConverted());
+            SubRoles.RemoveAll(AddON => AddON != role && AddON.IsBetrayalAddonV2());
             SubRoles.Remove(CustomRoles.Rascal);
             SubRoles.Remove(CustomRoles.Loyal);
-            SubRoles.Remove(CustomRoles.Admired);
         }
 
         switch (role)
@@ -230,10 +229,10 @@ public class PlayerState(byte playerId)
 
             case CustomRoles.Admired:
                 countTypes = CountTypes.Crew;
-                SubRoles.RemoveAll(AddON => AddON != role && AddON.IsConverted());
+                /*SubRoles.RemoveAll(AddON => AddON != role && AddON.IsConverted());
                 SubRoles.Remove(CustomRoles.Rascal);
                 SubRoles.Remove(CustomRoles.Loyal);
-                break;
+                break;*/
 
             case CustomRoles.Narc:
                 countTypes = CountTypes.Crew;
