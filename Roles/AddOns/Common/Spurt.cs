9f97b1db5beaa04638c27342ff2ecadc52ed3074
Tommy XL <fadeevvladimir16@gmail.com>
2025-01-31 00:28:51 +0800

    Fix Spurt spam "MarkDirtySettings" when player stands still

--- Roles/AddOns/Common/Spurt.cs
+++ Roles/AddOns/Common/Spurt.cs
@@ -92,7 +92,7 @@ internal class Spurt : IAddon
         if (!player.Is(CustomRoles.Spurt) || !player.IsAlive()) return;
 
         var pos = player.GetCustomPosition();
-        bool moving = Utils.GetDistance(pos, LastPos[player.PlayerId]) > 0f || player.MyPhysics.Animations.IsPlayingRunAnimation();
+        bool moving = Utils.GetDistance(pos, LastPos[player.PlayerId]) > 1f || player.MyPhysics.Animations.IsPlayingRunAnimation();
         LastPos[player.PlayerId] = pos;
 
         float modulator = Modulator.GetFloat();
