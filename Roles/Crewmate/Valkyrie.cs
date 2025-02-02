91190d3da3989343bb3ce1f22b594a6856dab29c
lime19844@gmail.com <lime19844@gmail.com>
2025-02-01 22:38:49 -0700

    final commit for 1.4.0

--- Roles/Crewmate/Valkyrie.cs
+++ Roles/Crewmate/Valkyrie.cs
@@ -7,9 +7,6 @@ internal class Valkyrie : RoleBase
 {
 
     private const int Id = 31100;
-    private static readonly HashSet<byte> playerIdList = [];
-    public static bool HasEnabled => playerIdList.Any();
-
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmateKilling;
     public override CustomRoles Role => CustomRoles.Valkyrie;
