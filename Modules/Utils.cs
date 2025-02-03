0af6a44652e6bd59509d8705b0517da1f5958199
Limeau <148590907+Limeau@users.noreply.github.com>
2025-02-02 23:06:44 -0700

    Revert "addons spawn chance as header"
    This reverts commit 34b55ec1009bacc46ba1d1c9f0014008541a8aba.

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -908,6 +908,11 @@ public static class Utils
             if (role.IsEnable())
             {
                 if (role is CustomRoles.Lovers) mode = GetChance(Options.LoverSpawnChances.GetInt());
+                else if (role.IsAdditionRole() && Options.CustomAdtRoleSpawnRate.TryGetValue(role, out var spawnRate))
+                {
+                    mode = GetChance(spawnRate.GetFloat());
+
+                }
                 var roleDisplay = $"{GetRoleName(role)}: {mode} x{role.GetCount()}";
                 if (role.IsAdditionRole()) addonsb.Add(roleDisplay);
                 else if (role.IsCrewmate()) crewsb.Add(roleDisplay);
