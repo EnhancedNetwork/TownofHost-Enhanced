1af0ec6b349fb2f88fda967712a3e7fc691af61e
Tommy XL <fadeevvladimir16@gmail.com>
2025-02-12 21:03:13 +0800

    Use SetHudActive In HudSpritePatch

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -496,13 +496,6 @@ internal class StartGameHostPatch
 
         EndOfSelectRolePatch:
 
-            try
-            {
-                if (!AmongUsClient.Instance.IsGameOver)
-                    DestroyableSingleton<HudManager>.Instance.SetHudActive(true);
-            }
-            catch { }
-
             foreach (var pc in PlayerControl.AllPlayerControls.GetFastEnumerator())
                 pc.ResetKillCooldown();
 
