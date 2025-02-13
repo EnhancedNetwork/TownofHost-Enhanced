65f3681b1854f69b7f79da878df158d8449c192b
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-13 14:09:32 -0700

    Update RoleBase.cs

--- Roles/Core/RoleBase.cs
+++ Roles/Core/RoleBase.cs
@@ -27,24 +27,24 @@ public abstract class RoleBase
         Init();
     }
 
-    public void OnAdd(byte playerid) // The player with the class executes this
+    public void OnAdd(byte playerid) // The Player with the class executes this
     {
         _state = Main.PlayerStates.Values.FirstOrDefault(state => state.PlayerId == playerid);
         try
         {
             CustomRoleManager.RoleClass.FirstOrDefault(r => r.Key == _state.MainRole).Value.IsEnable = true;
-            this.IsEnable = true; // Not supposed to be used, but some methods may have still implemented that check.
+            this.IsEnable = true;
         }
-        catch { } // temporary try catch
+        catch { }
 
 
         Add(playerid);
-        if (CustomRoleManager.OtherCollectionsSet) // If a role is applied mid-game, filter them again jsut in-case
+        if (CustomRoleManager.OtherCollectionsSet) // If a Role is applied mid-game, filter them again jsut in-case
         {
             CustomRoleManager.Add();
         }
 
-        // Remember desync player so that when changing role he will still be as desync
+        // Remember Desync Player so that when changing Role he will still be as Desync
         if (IsDesyncRole)
         {
             Main.DesyncPlayerList.Add(playerid);
@@ -62,18 +62,18 @@ public abstract class RoleBase
     { }
 
     /// <summary>
-    /// Variable resets when the game starts.
+    /// Variable resets when the game starts
     /// </summary>
     public virtual void Init()
     { }
     /// <summary>
-    /// When role is applied in the game, beginning or during the game.
+    /// When Role is applied in the game, beginning or during the game
     /// </summary>
     public virtual void Add(byte playerId)
     { }
 
     /// <summary>
-    /// If role has to be removed from player
+    /// If Role has to be removed from Player
     /// </summary>
     public virtual void Remove(byte playerId)
     { }
@@ -84,45 +84,42 @@ public abstract class RoleBase
     public abstract CustomRoles ThisRoleBase { get; }
 
     /// <summary>
-    /// Defines the role type
+    /// Defines the Role type
     /// </summary>
     public abstract Custom_RoleType ThisRoleType { get; }
 
     /// <summary>
-    /// Defines the custom role
+    /// Defines the custom Role
     /// </summary>
     public CustomRoles ThisCustomRole => Role;
 
-
-    //this is a draft, it is not usable yet, Imma fix it in another PR
-
     /// <summary>
-    /// A generic method to set if someone (desync Impostors) should see each-other on the reveal screen. (they will also not be able to kill eachother)
+    /// A generic method to set if someone (Desync Impostors) should see each-other on the reveal screen. (they will also not be able to kill eachother)
     /// </summary>
     public virtual void SetDesyncImpostorBuddies(ref Dictionary<PlayerControl, List<PlayerControl>> DesyncImpostorBuddy, PlayerControl caller)
     {
 
     }
     /// <summary>
-    /// A generic method to set if a impostor/Shapeshifter base may use Kill button.
+    /// A generic method to set if a Impostor/Shapeshifter base may use Kill button
     /// </summary>
     public virtual bool CanUseKillButton(PlayerControl pc) => pc.Is(Custom_Team.Impostor) && pc.IsAlive();
 
     /// <summary>
-    /// A generic method to set if a impostor/Shapeshifter base may Vent.
+    /// A generic method to set if a Impostor/Shapeshifter base may Vent
     /// </summary>
     public virtual bool CanUseImpostorVentButton(PlayerControl pc) => pc.Is(Custom_Team.Impostor) && pc.IsAlive();
 
     /// <summary>
-    /// A generic method to set if the role can use Sabotage.
+    /// A generic method to set if the Role can use Sabotage
     /// </summary>
     public virtual bool CanUseSabotage(PlayerControl pc) => pc.Is(Custom_Team.Impostor);
     /// <summary>
-    /// When the player presses the Sabotage button
+    /// When the Player presses the Sabotage button
     /// </summary>
     public virtual bool OnSabotage(PlayerControl pc) => pc != null;
     /// <summary>
-    /// When player is Enginner role base but should not move in Vents
+    /// When Player is Engineer base but should not move in Vents
     /// </summary>
     public virtual bool BlockMoveInVent(PlayerControl pc) => false;
 
@@ -134,7 +131,7 @@ public abstract class RoleBase
     public virtual void OnFixedUpdateLowLoad(PlayerControl pc)
     { }
     /// <summary>
-    /// A generic method to send a CustomRole's Gameoptions.
+    /// A generic method to send a CustomRole's Gameoptions
     /// </summary>
     public virtual void ApplyGameOptions(IGameOptions opt, byte playerId)
     {
@@ -150,7 +147,7 @@ public abstract class RoleBase
     /// <summary>
     /// A local method to check conditions during gameplay, 30 times each second
     /// </summary>
-    public virtual void OnFixedUpdate(PlayerControl player, bool lowLoad, long nowTime)
+    public virtual void OnFixedUpdate(PlayerControl player, bool lowLoad, long nowTime, int timerLowLoad)
     { }
 
     /// <summary>
@@ -158,12 +155,12 @@ public abstract class RoleBase
     /// </summary>
     public virtual bool OnTaskComplete(PlayerControl player, int completedTaskCount, int totalTaskCount) => true;
     /// <summary>
-    /// Other player complete a marked task
+    /// Other Player complete a marked task
     /// </summary>
-    public virtual void OnOthersTaskComplete(PlayerControl pc, PlayerTask task)
+    public virtual void OnOthersTaskComplete(PlayerControl pc, PlayerTask task, bool playerIsOverridden, PlayerControl realPlayer)
     { }
     /// <summary>
-    /// The role's tasks are needed for a task win
+    /// The Role's tasks are needed for a task win
     /// </summary>
     public virtual bool HasTasks(NetworkedPlayerInfo player, CustomRoles role, bool ForRecompute) => role.IsCrewmate() && !role.IsTasklessCrewmate() && (!ForRecompute || !player.Object.IsAnySubRole(x => x.IsConverted()));
 
@@ -173,77 +170,77 @@ public abstract class RoleBase
     public virtual bool OnCheckProtect(PlayerControl angel, PlayerControl target) => angel != null && target != null;
 
     /// <summary>
-    /// When role need force boot from Vent
+    /// When Role need force boot from Vent
     /// </summary>
     public virtual bool CheckBootFromVent(PlayerPhysics physics, int ventId) => physics == null;
     /// <summary>
-    /// A method for activating actions where the others roles starts playing an animation when entering a Vent
+    /// A method for activating actions where the others Roles starts playing an animation when entering a Vent
     /// </summary>
     public virtual bool OnCoEnterVentOthers(PlayerPhysics physics, int ventId) => physics == null;
     /// <summary>
-    /// A method for activating actions where the role starts playing an animation when entering a Vent
+    /// A method for activating actions where the Role starts playing an animation when entering a Vent
     /// </summary>
     public virtual void OnCoEnterVent(PlayerPhysics physics, int ventId)
     { }
     /// <summary>
-    /// A method for activating actions when role is already in Vent
+    /// A method for activating actions when Role is already in Vent
     /// </summary>
     public virtual void OnEnterVent(PlayerControl pc, Vent vent)
     { }
     /// <summary>
-    /// A generic method to activate actions once (CustomRole)player exists Vent.
+    /// A generic method to activate actions once (CustomRole) player exists Vent.
     /// </summary>
     public virtual void OnExitVent(PlayerControl pc, int ventId)
     { }
 
     /// <summary>
-    /// When role try fix any Sabotage or open doors
+    /// When Role try fix any Sabotage or open doors
     /// </summary>
     public virtual void UpdateSystem(ShipStatus __instance, SystemTypes systemType, byte amount, PlayerControl player)
     { }
     /// <summary>
-    /// When role try fix Electrical
+    /// When Role try fix Electrical
     /// </summary>
     public virtual void SwitchSystemUpdate(SwitchSystem __instance, byte amount, PlayerControl player)
     { }
 
     /// <summary>
-    ///  When role based on Impostors need force check target
+    ///  When Role based on Impostors need force check target
     /// </summary>
     public virtual bool ForcedCheckMurderAsKiller(PlayerControl killer, PlayerControl target) => target != null && killer != null;
     /// <summary>
-    /// When role the target requires a kill check
+    /// When Role the Target requires a kill check
     /// </summary>
     /// <returns>If the target doesn't require a kill cancel, always use "return true"</returns>
     public virtual bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target) => target != null && killer != null;
     /// <summary>
-    /// When role the target requires a kill check
+    /// When Role the Target requires a kill check
     /// </summary>
     /// <returns>If the target needs to cancel kill, always use "return true"</returns>
     public virtual bool CheckMurderOnOthersTarget(PlayerControl killer, PlayerControl target) => target == null || killer == null;
     /// <summary>
-    ///  When role the killer requires a kill check
+    ///  When Role the Killer requires a kill check
     /// </summary>
     public virtual bool OnCheckMurderAsKiller(PlayerControl killer, PlayerControl target) => target != null && killer != null;
 
     /// <summary>
-    /// When the killer murder his target
+    /// When the Killer murders Target
     /// </summary>
     public virtual void OnMurderPlayerAsKiller(PlayerControl killer, PlayerControl target, bool inMeeting, bool isSuicide)
     { }
     /// <summary>
-    /// When the target role died by killer
+    /// When the Target Role died by Killer
     /// </summary>
     public virtual void OnMurderPlayerAsTarget(PlayerControl killer, PlayerControl target, bool inMeeting, bool isSuicide)
     { }
 
     /// <summary>
-    /// When someone was died and need to run kill flash for specific role
+    /// When someone was died and need to run Kill Flash for specific Role
     /// </summary>
     public virtual bool KillFlashCheck(PlayerControl killer, PlayerControl target, PlayerControl seer) => false;
 
     /// <summary>
-    /// When the target role has died and kill flash needs to run globally
+    /// When the Target Role has died and Kill Flash needs to run globally
     /// </summary>
     public virtual bool GlobalKillFlashCheck(PlayerControl killer, PlayerControl target, PlayerControl seer) => false;
 
@@ -268,12 +265,12 @@ public abstract class RoleBase
     { }
 
 
-    // NOTE: when using UnShapeshift button, it will not be possible to revert to normal state because of complications.
-    // So OnCheckShapeShift and OnShapeshift are pointless when using it.
-    // Last thing, while the button may say "Shift" after resetability, the game still thinks you're Shapeshifted and will work instantly as intended.
+    // NOTE: when using UnShapeshift button, it will not be possible to revert to normal state because of complications
+    // So OnCheckShapeShift and OnShapeshift are pointless when using it
+    // Last thing, while the button may say "Shift" after resetability, the game still thinks you're Shapeshifted and will work instantly as intended
 
     /// <summary>
-    /// A method which when implemented automatically makes the players always Shapeshifted (as themselves). Inside you can put functions to happen when "Un-Shapeshift" button is pressed.
+    /// A method which when implemented automatically makes Players always Shapeshifted (as themselves). Inside you can put functions to happen when "Un-Shapeshift" button is pressed.
     /// </summary>
     [Obfuscation(Exclude = true)]
     public virtual void UnShapeShiftButton(PlayerControl shapeshifter) { }
@@ -289,43 +286,43 @@ public abstract class RoleBase
 
     /// <summary>
     /// When the meeting was start by report dead body or press meeting button
-    /// target is null when meeting was start by pressing meeting button
-    /// target is not null when meeting was start by report dead body
-    /// When target left the game, it's data in NetworkedPlayerInfo is not null, it still has data that can be used
+    /// Target is null when meeting was start by pressing meeting button
+    /// Target is not null when meeting was start by report dead body
+    /// When Target left the game, it's data in NetworkedPlayerInfo is not null, it still has data that can be used
     /// But if you use target.Object, then it can be null
     /// </summary>
     public virtual void OnReportDeadBody(PlayerControl reporter, NetworkedPlayerInfo target)
     { }
 
     /// <summary>
-    /// When guesser need check guess (check limit or cannot guess а Role/Add-on)
+    /// When Guesser need check guess (check limit or cannot guess а Role/Add-on)
     /// </summary>
     public virtual bool GuessCheck(bool isUI, PlayerControl guesser, PlayerControl target, CustomRoles role, ref bool guesserSuicide) => target == null;
     /// <summary>
-    /// When guesser trying guess target a role
-    /// Target need to check whether misguessed so it wont cancel mis guesses.
+    /// When Guesser trying guess target a Role
+    /// Target need to check whether misguessed so it wont cancel misguesses
     /// </summary>
     public virtual bool OnRoleGuess(bool isUI, PlayerControl target, PlayerControl guesser, CustomRoles role, ref bool guesserSuicide) => target == null;
 
     /// <summary>
-    /// When guesser was misguessed
+    /// When Guesser was misguessed
     /// </summary>
     public virtual bool CheckMisGuessed(bool isUI, PlayerControl guesser, PlayerControl target, CustomRoles role, ref bool guesserSuicide) => target == null;
 
     /// <summary>
-    /// Check exile role
+    /// Check exile Role
     /// </summary>
     public virtual void CheckExile(NetworkedPlayerInfo exiled, ref bool DecidedWinner, bool isMeetingHud, ref string name)
     { }
 
     /// <summary>
-    /// Check exile target
+    /// Check exile Target
     /// </summary>
     public virtual void CheckExileTarget(NetworkedPlayerInfo exiled, ref bool DecidedWinner, bool isMeetingHud, ref string name)
     { }
 
     /// <summary>
-    /// When player was exiled
+    /// When Player was exiled
     /// </summary>
     public virtual void OnPlayerExiled(PlayerControl player, NetworkedPlayerInfo exiled)
     { }
@@ -351,23 +348,23 @@ public abstract class RoleBase
     public virtual string PVANameText(PlayerVoteArea pva, PlayerControl seer, PlayerControl target) => string.Empty;
 
     /// <summary>
-    /// Used when player should be dead after meeting
+    /// Used when Player should be dead after meeting
     /// </summary>
     public virtual void OnCheckForEndVoting(PlayerState.DeathReason deathReason, params byte[] exileIds)
     { }
     /// <summary>
-    /// Notify a specific role about something after the meeting was ended.
+    /// Notify a specific Role about something after the meeting was ended
     /// </summary>
     public virtual void NotifyAfterMeeting()
     { }
     /// <summary>
-    /// A generic method to activate actions after a meeting has ended.
+    /// A generic method to activate actions after a meeting has ended
     /// </summary>
     public virtual void AfterMeetingTasks()
     { }
 
     /// <summary>
-    /// When player left the game
+    /// When Player left the game
     /// </summary>
     //public virtual void OnPlayerLeft(ClientData clientData) Note: instead "OnPlayerLeft" use "OnMurderPlayer" and "isSuicide"
     //{ }
@@ -380,18 +377,18 @@ public abstract class RoleBase
 
 
     /// <summary>
-    /// If role wants to return the vote to the player during meeting. Can also work to check any abilities during meeting.
+    /// If Role wants to return the vote to the Player during meeting. Can also work to check any abilities during meeting
     /// </summary>
     [Obfuscation(Exclude = true)]
     public virtual bool CheckVote(PlayerControl voter, PlayerControl target) => voter != null && target != null;
 
     /// <summary>
-    /// A check for any role abilites of the player which voted, when the vote hasn't been canceled by any other means.
+    /// A check for any Role abilites of the Player which voted, when the vote hasn't been canceled by any other means
     /// </summary>
     public virtual void OnVote(PlayerControl votePlayer, PlayerControl voteTarget)
     { }
     /// <summary>
-    /// A check for any role abilites of the player that was voted, when the vote hasn't been canceled by any other means.
+    /// A check for any Role abilites of the Player that was voted, when the vote hasn't been canceled by any other means
     /// </summary>
     public virtual void OnVoted(PlayerControl votedPlayer, PlayerControl votedTarget)
     { }
@@ -407,7 +404,7 @@ public abstract class RoleBase
     public virtual void AddVisualVotes(PlayerVoteArea votedPlayer, ref List<MeetingHud.VoterState> statesList)
     { }
     /// <summary>
-    /// Add real votes num
+    /// Add real number votes
     /// </summary>
     public virtual int AddRealVotesNum(PlayerVoteArea PVA) => 0;
 
@@ -424,19 +421,16 @@ public abstract class RoleBase
     public virtual Sprite GetAbilityButtonSprite(PlayerControl player, bool shapeshifting) => null;
     public virtual Sprite ImpostorVentButtonSprite(PlayerControl player) => null;
     public virtual Sprite ReportButtonSprite { get; }
+    public virtual Sprite SabotageButtonSprite { get; }
 
     /// <summary>
-    /// Set PlayerName text for the role
+    /// Set PlayerName text for the Role
     /// </summary>
     public virtual string NotifyPlayerName(PlayerControl seer, PlayerControl target, string TargetPlayerName = "", bool IsForMeeting = false) => string.Empty;
-    //
-    //
-    // Add Mark/LowerText/Suffix for player
-    // When using this code remember the seer can also see the target, therefore..
-    //
-    // return string.empty if "seer != seen" if only seer should have it
-    // otherwise make some list or byte or smt of sorts to only get the target.
-    // not needed if both should have it.
+    // Add Mark/LowerText/Suffix for Player
+    // When using this code remember the Seer can also see the Target, therefore..
+    // return string.empty if "seer != seen" if only Seer should have it
+    // otherwise make some list or byte or smt of sorts to only get the Target
     public virtual string GetMark(PlayerControl seer, PlayerControl seen, bool isForMeeting = false) => string.Empty;
     public virtual string GetLowerText(PlayerControl seer, PlayerControl seen = null, bool isForMeeting = false, bool isForHud = false) => string.Empty;
     public virtual string GetSuffix(PlayerControl seer, PlayerControl seen, bool isForMeeting = false) => string.Empty;
@@ -449,11 +443,9 @@ public abstract class RoleBase
         return sb.ToString();
     }
 
-    // 
     // IMPORTANT note about otherIcons: 
     // These are only called once in the method, so object attributes are banned (as 99.99% of roles only want the method to run once).
     // You may use static attributes, tho you can simply just use utils.GetRoleBasesByType<> if need be.
-    //
     public virtual string GetMarkOthers(PlayerControl seer, PlayerControl seen, bool isForMeeting = false) => string.Empty;
     public virtual string GetLowerTextOthers(PlayerControl seer, PlayerControl seen = null, bool isForMeeting = false, bool isForHud = false) => string.Empty;
     public virtual string GetSuffixOthers(PlayerControl seer, PlayerControl seen, bool isForMeeting = false) => string.Empty;
@@ -474,6 +466,7 @@ public abstract class RoleBase
         // Ability
         Cooldown,
         AbilityCooldown,
+        AbilityDuration,
         SkillLimitTimes,
 
         // Impostor-based settings
