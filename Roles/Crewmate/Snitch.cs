1cc5c12d7dc6942830e2eb1a683979f359e2e995
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-13 22:24:17 -0700

    Update Snitch.cs

--- Roles/Crewmate/Snitch.cs
+++ Roles/Crewmate/Snitch.cs
@@ -14,7 +14,6 @@ internal class Snitch : RoleBase
     private const int Id = 9500;
     private static readonly HashSet<byte> playerIdList = [];
     public static bool HasEnabled => playerIdList.Any();
-
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmateSupport;
     //==================================================================\\
