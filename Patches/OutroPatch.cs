181c3310e86628e990e7ac5bcef7ae50994ed821
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-14 08:51:01 -0700

    Update OutroPatch.cs

--- Patches/OutroPatch.cs
+++ Patches/OutroPatch.cs
@@ -118,7 +118,7 @@ class EndGamePatch
         {
             if (Options.CurrentGameMode != CustomGameMode.Standard) break;
             if (kvp.Value.MainRoleLogs.Where(x => !x.Item2.IsVanilla()).ToList().Count <= 1) continue;
-            sb2.Append($"\n[{kvp.Key}] {Main.AllPlayerNames[kvp.Key]} {Utils.GetDisplayRoleAndSubName(kvp.Key, kvp.Key)}");
+            sb2.Append($"\n[{kvp.Key}] {Main.AllPlayerNames[kvp.Key]} {Utils.GetDisplayRoleAndSubName(kvp.Key, kvp.Key, false, false)}");
             foreach (var item in kvp.Value.MainRoleLogs.OrderBy(x => x.Item1.Ticks))
             {
                 if (item.Item2.IsVanilla()) continue;
