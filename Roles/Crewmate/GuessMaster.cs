948cc38a72c0b92979b80041c84c92b63fd0c0b5
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-13 22:21:43 -0700

    Update GuessMaster.cs

--- Roles/Crewmate/GuessMaster.cs
+++ Roles/Crewmate/GuessMaster.cs
@@ -9,7 +9,6 @@ internal class GuessMaster : RoleBase
     private const int Id = 26800;
     private static readonly HashSet<byte> playerIdList = [];
     public static bool HasEnabled => playerIdList.Any();
-
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmateBasic;
     //==================================================================\\
