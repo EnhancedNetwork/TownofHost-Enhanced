82f8b967418dee95092e1e6951d8bd0042d4cfcd
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-13 22:24:38 -0700

    Update Technician.cs

--- Roles/Crewmate/Technician.cs
+++ Roles/Crewmate/Technician.cs
@@ -12,7 +12,6 @@ internal class Technician : RoleBase
     private static readonly HashSet<byte> playerIdList = [];
     public static bool HasEnabled => playerIdList.Any();
     public override CustomRoles Role => CustomRoles.Technician;
-
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmatePower;
     //==================================================================\\
