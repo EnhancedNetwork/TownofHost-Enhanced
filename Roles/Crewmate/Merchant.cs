319d64844bbe0c49f1154f642c3e33990d33217c
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-15 14:04:54 -0800

    Update Merchant.cs

--- Roles/Crewmate/Merchant.cs
+++ Roles/Crewmate/Merchant.cs
@@ -139,7 +139,7 @@ internal class Merchant : RoleBase
                 )
             ).ToList();
 
-        if (AllAlivePlayer.Any())
+        if (AllAlivePlayer.Count > 0)
         {
             bool helpfulAddon = GroupedAddons[AddonTypes.Helpful].Contains(addon);
             bool harmfulAddon = GroupedAddons[AddonTypes.Harmful].Contains(addon);
@@ -161,6 +161,12 @@ internal class Merchant : RoleBase
                 ).ToList();
             }
 
+            if (AllAlivePlayer.Count < 1)
+            {
+                SellFail(player);
+                return true;
+            }
+
             PlayerControl target = AllAlivePlayer.RandomElement();
 
             target.RpcSetCustomRole(addon);
@@ -173,9 +179,14 @@ internal class Merchant : RoleBase
         }
         else
         {
+            SellFail(player);
+            return true;
+        }
+
+        static void SellFail(PlayerControl player)
+        {
             player.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Merchant), GetString("MerchantAddonSellFail")));
             Logger.Info("All Alive Player Count = 0", "Merchant");
-            return true;
         }
 
         return true;
