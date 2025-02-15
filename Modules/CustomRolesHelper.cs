be31e866002b5261426ceed6516c4a267adddc0d
ImpyIsLazy <impostoramongus71@gmail.com>
2025-02-15 21:36:26 +0800

    Update CustomRolesHelper.cs

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -5,6 +5,7 @@ using TOHE.Roles.AddOns.Crewmate;
 using TOHE.Roles.AddOns.Impostor;
 using TOHE.Roles.Core;
 using TOHE.Roles.Crewmate;
+using TOHE.Roles.Double;
 using TOHE.Roles.Impostor;
 using TOHE.Roles.Neutral;
 using TOHE.Roles.Coven;
