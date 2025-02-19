f321c7f4a1bb4b80969872c98e476f5549edaf6b
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-19 10:43:27 -0700

    Update Solsticer.cs

--- Roles/Neutral/Solsticer.cs
+++ Roles/Neutral/Solsticer.cs
@@ -123,7 +123,7 @@ internal class Solsticer : RoleBase
         }
         warningActived = true;
         SendRPC();
-        Utils.NotifyRoles(ForceLoop: true);
+        Utils.NotifyRoles(SpecifyTarget: pc);
     }
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
     {
