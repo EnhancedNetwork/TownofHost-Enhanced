479125d94142490cc6d87f95a8a246daa4f9c606
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-19 07:54:16 -0700

    Update onGameStartedPatch.cs

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -147,15 +147,6 @@ internal class ChangeRoleSettings
                 }
             }
 
-            foreach (var target in Main.AllPlayerControls)
-            {
-                foreach (var seer in Main.AllPlayerControls)
-                {
-                    var pair = (target.PlayerId, seer.PlayerId);
-                    Main.LastNotifyNames[pair] = target.name;
-                }
-            }
-
             foreach (var pc in Main.AllPlayerControls)
             {
                 var outfit = pc.Data.DefaultOutfit;
@@ -178,6 +169,14 @@ internal class ChangeRoleSettings
                         pc.RpcSetName(realName);
                     }
                 }
+
+                foreach (var target in Main.AllPlayerControls)
+                {
+                    var pair = (target.PlayerId, pc.PlayerId);
+                    Main.LastNotifyNames[pair] = currentName;
+                }
+
+                Main.LowLoadUpdateName[pc.PlayerId] = true;
 
                 Main.PlayerStates[pc.PlayerId] = new(pc.PlayerId)
                 {
