5e8b0b843c113d9d08ef40ca4d11d4cff9dfa869
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-19 07:31:14 -0700

    Update Utils.cs

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1868,7 +1868,24 @@ public static class Utils
 
         return baseMethod.DeclaringType != derivedMethod.DeclaringType;
     }
+    public static System.Collections.IEnumerator NotifyEveryoneAsync(int speed = 2)
+    {
+        var count = 0;
+        bool isMeeting = GameStates.IsMeeting;
+        if (isMeeting) yield break;
 
+        PlayerControl[] aapc = Main.AllAlivePlayerControls;
+
+        foreach (PlayerControl seer in aapc)
+        {
+            foreach (PlayerControl target in aapc)
+            {
+                if (isMeeting) yield break;
+                NotifyRoles(SpecifySeer: seer, SpecifyTarget: target);
+                if (count++ % speed == 0) yield return null;
+            }
+        }
+    }
     // During intro scene to set team name and Role info for non-modded clients and skip the rest
     // Note: When Neutral is based on the Crewmate Role then it is impossible to display the info for it
     // If not a Desync Role remove team display
@@ -1923,7 +1940,7 @@ public static class Utils
         var SelfName = $"{SelfTeamName}{SelfRoleName}{SelfSubRolesName}\r\n{RoleInfo}{RoleNameUp}";
 
         // Privately sent name
-        player.RpcSetNamePrivate(SelfName, player);
+        player.RpcSetNamePrivate(SelfName, player, force: true);
     }
 
     public static NetworkedPlayerInfo GetPlayerInfoById(int PlayerId) =>
