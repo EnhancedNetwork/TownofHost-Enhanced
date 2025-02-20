615a6f66c686ef4a0d0725b12aa9e22fd91ec5f3
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-19 18:42:27 -0700

    Update Camouflager.cs

--- Roles/Impostor/Camouflager.cs
+++ Roles/Impostor/Camouflager.cs
@@ -115,7 +115,7 @@ internal class Camouflager : RoleBase
         {
             if (!Main.MeetingIsStarted && GameStates.IsInTask)
             {
-                Camouflage.CheckCamouflage(false);
+                Camouflage.CheckCamouflage();
             }
         }, timer, "Camouflager Use Shapeshift");
     }
