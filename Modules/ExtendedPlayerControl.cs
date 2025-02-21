6f20f539797f3e7989dd7eab1615457522c27726
ImpyIsLazy <impostoramongus71@gmail.com>
2025-02-21 23:38:56 +0800

    Update ExtendedPlayerControl.cs

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -1372,10 +1372,10 @@ static class ExtendedPlayerControl
         else if (Altruist.HasEnabled && seer.IsMurderedThisRound()) return false;
         else if (seer.GetCustomRole() == target.GetCustomRole() && seer.GetCustomRole().IsNK()) return true;
         else if (Options.LoverKnowRoles.GetBool() && seer.Is(CustomRoles.Lovers) && target.Is(CustomRoles.Lovers)) return true;
-        else if (Options.ImpsCanSeeEachOthersRoles.GetBool() && seer.CheckMMCanSeeImp() && target.CheckMMCanSeeImp() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
-        else if (Madmate.MadmateKnowWhosImp.GetBool() && seer.Is(CustomRoles.Madmate) && target.CheckMMCanSeeImp() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer) return true;
-        else if (Madmate.ImpKnowWhosMadmate.GetBool() && target.Is(CustomRoles.Madmate) && seer.CheckMMCanSeeImp() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
-        else if (seer.CheckMMCanSeeImp() && target.GetCustomRole().IsGhostRole() && target.GetCustomRole().IsImpostor() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
+        else if (Options.ImpsCanSeeEachOthersRoles.GetBool() && seer.CheckImpTeamCanSeeTeammates() && target.CheckImpTeamCanSeeTeammates() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
+        else if (Madmate.MadmateKnowWhosImp.GetBool() && seer.Is(CustomRoles.Madmate) && target.CheckImpTeamCanSeeTeammates() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer) return true;
+        else if (Madmate.ImpKnowWhosMadmate.GetBool() && target.Is(CustomRoles.Madmate) && seer.CheckImpTeamCanSeeTeammates() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
+        else if (seer.CheckImpTeamCanSeeTeammates() && target.GetCustomRole().IsGhostRole() && target.GetCustomRole().IsImpostor() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer && !seer.Is(CustomRoles.Narc)) return true;
         else if (seer.IsNeutralApocalypse() && target.IsNeutralApocalypse() && !Main.PlayerStates[seer.PlayerId].IsNecromancer && !Main.PlayerStates[target.PlayerId].IsNecromancer) return true;
         else if (Ritualist.EnchantedKnowsCoven.GetBool() && seer.Is(CustomRoles.Enchanted) && target.Is(Custom_Team.Coven)) return true;
         else if (target.Is(CustomRoles.Enchanted) && seer.Is(Custom_Team.Coven)) return true;
@@ -1414,7 +1414,7 @@ static class ExtendedPlayerControl
         if (seer.PlayerId == target.PlayerId) return true;
         else if (seer.Is(CustomRoles.GM) || target.Is(CustomRoles.GM) || seer.Is(CustomRoles.God) || (seer.AmOwner && Main.GodMode.Value)) return true;
         else if (Options.SeeEjectedRolesInMeeting.GetBool() && Main.PlayerStates[target.PlayerId].deathReason == PlayerState.DeathReason.Vote && subRole.IsBetrayalAddonV2()) return true;
-        else if (Options.ImpsCanSeeEachOthersAddOns.GetBool() && seer.CheckMMCanSeeImp() && !seer.Is(CustomRoles.Narc) && target.CheckMMCanSeeImp() && !subRole.IsBetrayalAddon() && subRole is not CustomRoles.Torch) return true;
+        else if (Options.ImpsCanSeeEachOthersAddOns.GetBool() && seer.CheckImpTeamCanSeeTeammates() && !seer.Is(CustomRoles.Narc) && target.CheckImpTeamCanSeeTeammates() && !subRole.IsBetrayalAddon() && subRole is not CustomRoles.Torch) return true;
         else if (Options.CovenCanSeeEachOthersAddOns.GetBool() && seer.Is(Custom_Team.Coven) && target.Is(Custom_Team.Coven) && !subRole.IsBetrayalAddon()) return true;
         else if (Options.ApocCanSeeEachOthersAddOns.GetBool() && seer.IsNeutralApocalypse() && target.IsNeutralApocalypse() && !subRole.IsBetrayalAddon()) return true;
         
@@ -1448,7 +1448,7 @@ static class ExtendedPlayerControl
     {
         //if (seer.GetRoleClass().KnowRoleTarget(seer, target)) return true;
 
-        if (seer.CheckMMCanSeeImp( ) && !seer.Is(CustomRoles.Narc))
+        if (seer.CheckImpTeamCanSeeTeammates() && !seer.Is(CustomRoles.Narc))
         {
             // Imp know Madmate
             if (target.Is(CustomRoles.Madmate) && Madmate.ImpKnowWhosMadmate.GetBool())
