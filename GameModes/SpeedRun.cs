c99f9241a5e71687efbd4717add6a2a00354f279
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-02-28 09:29:29 +0800

    Role Basis Change

--- GameModes/SpeedRun.cs
+++ GameModes/SpeedRun.cs
@@ -11,9 +11,10 @@ public static class SpeedRun
     public static OptionItem SpeedRun_NumShortTasks;
     public static OptionItem SpeedRun_NumLongTasks;
 
+    public static OptionItem SpeedRun_RunnerNormalSpeed;
     public static OptionItem SpeedRun_RunnerKcd;
     public static OptionItem SpeedRun_RunnerKcdPerDeadPlayer;
-    public static OptionItem SpeedRun_RunnerSpeedAfterFinishGame;
+    public static OptionItem SpeedRun_RunnerSpeedAfterFinishTasks;
     public static OptionItem SpeedRun_AllowCloseDoor;
 
     public static OptionItem SpeedRun_ArrowPlayers;
@@ -38,40 +39,43 @@ public static class SpeedRun
         SpeedRun_NumLongTasks = IntegerOptionItem.Create(Id + 3, "SpeedRun_NumLongTasks", new(1, 15, 1), 1, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun);
 
-        SpeedRun_RunnerKcd = FloatOptionItem.Create(Id + 4, "SpeedRun_RunnerKcd", new(0.5f, 60f, 0.5f), 15f, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun)
-            .SetValueFormat(OptionFormat.Seconds);
-        SpeedRun_RunnerKcdPerDeadPlayer = FloatOptionItem.Create(Id + 5, "SpeedRun_RunnerKcdPerDeadPlayer", new(0f, 60f, 0.1f), 0f, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun)
-            .SetValueFormat(OptionFormat.Seconds);
-        SpeedRun_RunnerSpeedAfterFinishGame = FloatOptionItem.Create(Id + 6, "SpeedRun_RunnerSpeedAfterFinishGame", new(0.25f, 5f, 0.25f), 2.5f, TabGroup.ModSettings, false)
+        SpeedRun_RunnerNormalSpeed = FloatOptionItem.Create(Id + 4, "SpeedRun_RunnerNormalSpeed", new(0.5f, 60f, 0.5f), 15f, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun)
             .SetValueFormat(OptionFormat.Multiplier);
-        SpeedRun_AllowCloseDoor = BooleanOptionItem.Create(Id + 7, "SpeedRun_AllowCloseDoor", false, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun);
-
-        SpeedRun_ArrowPlayers = BooleanOptionItem.Create(Id + 8, "SpeedRun_ArrowPlayers", true, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun);
-        SpeedRun_ArrowPlayersPlayerLiving = IntegerOptionItem.Create(Id + 9, "SpeedRun_ArrowPlayersPlayerLiving", new(1, 127, 1), 1, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun);
-
-        SpeedRun_SpeedBoostAfterTask = BooleanOptionItem.Create(Id + 10, "SpeedRun_SpeedBoostAfterTask", true, TabGroup.ModSettings, false)
-            .SetGameMode(CustomGameMode.SpeedRun);
-        SpeedRun_SpeedBoostSpeed = FloatOptionItem.Create(Id + 11, "SpeedRun_SpeedBoostSpeed", new(0.25f, 5f, 0.25f), 2.5f, TabGroup.ModSettings, false)
+        SpeedRun_RunnerKcd = FloatOptionItem.Create(Id + 5, "SpeedRun_RunnerKcd", new(0.5f, 60f, 0.5f), 15f, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun)
+            .SetValueFormat(OptionFormat.Seconds);
+        SpeedRun_RunnerKcdPerDeadPlayer = FloatOptionItem.Create(Id + 6, "SpeedRun_RunnerKcdPerDeadPlayer", new(0f, 60f, 0.1f), 0f, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun)
+            .SetValueFormat(OptionFormat.Seconds);
+        SpeedRun_RunnerSpeedAfterFinishTasks = FloatOptionItem.Create(Id + 7, "SpeedRun_RunnerSpeedAfterFinishTasks", new(0.25f, 5f, 0.25f), 2.5f, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun)
             .SetValueFormat(OptionFormat.Multiplier);
-        SpeedRun_SpeedBoostDuration = FloatOptionItem.Create(Id + 12, "SpeedRun_SpeedBoostDuration", new(0.5f, 60f, 0.5f), 2.5f, TabGroup.ModSettings, false)
+        SpeedRun_AllowCloseDoor = BooleanOptionItem.Create(Id + 8, "SpeedRun_AllowCloseDoor", false, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun);
+
+        SpeedRun_ArrowPlayers = BooleanOptionItem.Create(Id + 9, "SpeedRun_ArrowPlayers", true, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun);
+        SpeedRun_ArrowPlayersPlayerLiving = IntegerOptionItem.Create(Id + 10, "SpeedRun_ArrowPlayersPlayerLiving", new(1, 127, 1), 1, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun);
+
+        SpeedRun_SpeedBoostAfterTask = BooleanOptionItem.Create(Id + 11, "SpeedRun_SpeedBoostAfterTask", true, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun);
+        SpeedRun_SpeedBoostSpeed = FloatOptionItem.Create(Id + 12, "SpeedRun_SpeedBoostSpeed", new(0.25f, 5f, 0.25f), 2.5f, TabGroup.ModSettings, false)
+            .SetGameMode(CustomGameMode.SpeedRun)
+            .SetValueFormat(OptionFormat.Multiplier);
+        SpeedRun_SpeedBoostDuration = FloatOptionItem.Create(Id + 13, "SpeedRun_SpeedBoostDuration", new(0.5f, 60f, 0.5f), 2.5f, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun)
             .SetValueFormat(OptionFormat.Seconds);
 
-        SpeedRun_ProtectAfterTask = BooleanOptionItem.Create(Id + 13, "SpeedRun_ProtectAfterTask", true, TabGroup.ModSettings, false)
+        SpeedRun_ProtectAfterTask = BooleanOptionItem.Create(Id + 14, "SpeedRun_ProtectAfterTask", true, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun);
-        SpeedRun_ProtectDuration = FloatOptionItem.Create(Id + 14, "SpeedRun_ProtectDuration", new(0.5f, 60f, 0.5f), 2.5f, TabGroup.ModSettings, false)
+        SpeedRun_ProtectDuration = FloatOptionItem.Create(Id + 15, "SpeedRun_ProtectDuration", new(0.5f, 60f, 0.5f), 2.5f, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun)
             .SetValueFormat(OptionFormat.Seconds);
-        SpeedRun_ProtectOnlyOnce = BooleanOptionItem.Create(Id + 15, "SpeedRun_ProtectOnlyOnce", true, TabGroup.ModSettings, false)
+        SpeedRun_ProtectOnlyOnce = BooleanOptionItem.Create(Id + 16, "SpeedRun_ProtectOnlyOnce", true, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun);
-        SpeedRun_ProtectKcd = FloatOptionItem.Create(Id + 16, "SpeedRun_ProtectKcd", new(0.5f, 60f, 0.5f), 5f, TabGroup.ModSettings, false)
+        SpeedRun_ProtectKcd = FloatOptionItem.Create(Id + 17, "SpeedRun_ProtectKcd", new(0.5f, 60f, 0.5f), 5f, TabGroup.ModSettings, false)
             .SetGameMode(CustomGameMode.SpeedRun)
             .SetValueFormat(OptionFormat.Seconds);
     }
@@ -80,7 +84,7 @@ public static class SpeedRun
 public class Runner : RoleBase
 {
     public override CustomRoles Role => CustomRoles.Runner;
-    public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
+    public override CustomRoles ThisRoleBase => BasisChanged ? CustomRoles.Impostor : CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.None;
     public override bool IsDesyncRole => true;
 
@@ -113,6 +117,7 @@ public class Runner : RoleBase
             if (SpeedBoostState.Item2 <= 0)
             {
                 SpeedBoostState = (false, 0f);
+                player.MarkDirtySettings();
             }
         }
     }
@@ -155,7 +160,7 @@ public class Runner : RoleBase
         if (SpeedRun.SpeedRun_SpeedBoostAfterTask.GetBool() && !BasisChanged)
         {
             SpeedBoostState = (true, SpeedRun.SpeedRun_SpeedBoostDuration.GetFloat());
-            player.SyncSettings();
+            player.MarkDirtySettings();
         }
 
         if (SpeedRun.SpeedRun_ProtectAfterTask.GetBool() && !BasisChanged)
@@ -164,11 +169,11 @@ public class Runner : RoleBase
             player.RpcSpecificProtectPlayer(player, player.CurrentOutfit.ColorId);
         }
 
-        if (completedTaskCount >= totalTaskCount && !BasisChanged)
+        if (completedTaskCount >= totalTaskCount && !BasisChanged && player.IsAlive())
         {
             BasisChanged = true;
             player.RpcChangeRoleBasis(CustomRoles.Runner);
-            player.SyncSettings();
+            player.MarkDirtySettings();
         }
 
         return true;
