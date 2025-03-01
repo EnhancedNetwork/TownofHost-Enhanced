755b3e7ec92fef98760561d7cef42fe7b4e731a7
Limeau <148590907+Limeau@users.noreply.github.com>
2025-03-01 14:53:30 -0700

    fix Bankrupt banned by EAC for sabotaging

--- Roles/Neutral/Bankrupt.cs
+++ Roles/Neutral/Bankrupt.cs
@@ -6,4 +6,7 @@ internal class Bankrupt : RoleBase
     public override bool IsDesyncRole => true;
     public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralBenign;
+    public override bool CanUseKillButton(PlayerControl player) => false;
+    public override bool CanUseImpostorVentButton(PlayerControl player) => false;
+    public override bool CanUseSabotage(PlayerControl player) => false;
 }
