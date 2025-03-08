1def9ef554e85fb3d7c5bb6adb3f3b33080e4055
Tommy XL <fadeevvladimir16@gmail.com>
2025-03-09 02:10:13 +0800

    Alpha 2 Hotfix 2

--- main.cs
+++ main.cs
@@ -46,12 +46,12 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2025.0306.230.000021"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.3.0 Alpha 2.1";
+    public const string PluginVersion = "2025.0308.230.000022"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.3.0 Alpha 2.2";
     public const string SupportedVersionAU = "2024.10.29"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool devRelease = false; // Latest: V2.3.0 Alpha 2 Hotfix 1
+    public static readonly bool devRelease = false; // Latest: V2.3.0 Alpha 2 Hotfix 2
     public static readonly bool canaryRelease = true; // Latest: V2.2.0 Beta 4
     public static readonly bool fullRelease = false; // Latest: V2.2.0
 
