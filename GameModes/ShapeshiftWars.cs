7d86027fbad9779b46dca7d697f9af1733807c53
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-03-19 21:38:02 +0800

    All Options

--- GameModes/ShapeshiftWars.cs
+++ GameModes/ShapeshiftWars.cs
@@ -10,7 +10,30 @@ using static TOHE.Utils;
 namespace TOHE;
 public static class ShapeshiftWars
 {
+    public static OptionItem SW_NeedToDoTasksToKill;
+    public static OptionItem SW_CommonTasks;
+    public static OptionItem SW_ShortTasks;
+    public static OptionItem SW_LongTasks;
+    public static OptionItem SW_GenerateSameTasks;
 
+    public static OptionItem SW_NormalSpeed;
+    public static OptionItem SW_ShapeShiftSpeed;
+    public static OptionItem SW_BoostWhenBeTarget;
+    public static OptionItem SW_BoostWhenKill;
+
+    public static OptionItem SW_FirstSSCoolDown;
+    public static OptionItem SW_ShapeshiftCooldown;
+    public static OptionItem SW_ShapeshiftDuration;
+    public static OptionItem SW_ShapeshiftAnimation;
+
+    public static OptionItem SW_NormalKcd;
+    public static OptionItem SW_MisClickResetKcd;
+    public static OptionItem SW_MisClickKcd;
+    public static OptionItem SW_AllowMiskill;
+    public static OptionItem SW_MissKillNextSSCoolDown;
+
+    public static OptionItem SW_SsSeeTargetArrow;
+    public static OptionItem SW_SsTargetSeeSArror;
 }
 
 public class Shifter : RoleBase
@@ -21,4 +44,3 @@ public class Shifter : RoleBase
 
     public override Custom_RoleType ThisRoleType => Custom_RoleType.None;
 }
-
