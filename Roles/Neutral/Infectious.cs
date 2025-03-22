0b18f63c096d9da49b70e81d5976e664333819f6
ImpyIsLazy <impostoramongus71@gmail.com>
2025-03-22 23:30:43 +0800

    Update Infectious.cs

--- Roles/Neutral/Infectious.cs
+++ Roles/Neutral/Infectious.cs
@@ -165,10 +165,14 @@ internal class Infectious : RoleBase
     public static bool CanBeBitten(PlayerControl pc)
     {
         return pc != null && (pc.GetCustomRole().IsCrewmate()
-            || pc.GetCustomRole().IsImpostorTeamV3()
+            || pc.GetCustomRole().IsImpostor()
             || pc.GetCustomRole().IsNK()
-            || pc.GetCustomRole().IsCoven())
-            && !pc.Is(CustomRoles.Infectious) && !pc.Is(CustomRoles.Virus) && !pc.IsTransformedNeutralApocalypse();
+            || pc.GetCustomRole().IsCoven()) && !pc.Is(CustomRoles.Infected)
+            && !pc.Is(CustomRoles.Admired)
+            && !pc.Is(CustomRoles.Loyal)
+            && !pc.Is(CustomRoles.Cultist)
+            && !pc.Is(CustomRoles.Enchanted)
+            && !pc.Is(CustomRoles.Infectious) && !pc.Is(CustomRoles.Virus) && !pc.IsTransformedNeutralApocalypse() && !(CovenManager.HasNecronomicon(pc.PlayerId) && pc.Is(CustomRoles.CovenLeader));
     }
     public override void SetAbilityButtonText(HudManager hud, byte playerId)
     {
