5c1191d8f8cc7f50e9d793ce529efeaeca8c2888
ImpyIsLazy <impostoramongus71@gmail.com>
2025-03-22 11:51:36 +0800

    changes + add some more checks to IsPlayerXTeam

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -469,6 +469,9 @@ public static class CustomRolesHelper
 
         //settings disabled,hurried cant be recruited
         else if (target.Is(CustomRoles.Hurried) && !Hurried.CanBeConverted.GetBool()) return false;
+
+        //Coven Leader cant be recruited when they have the necronomicon
+        else if (CovenManager.HasNecronomicon(target.PlayerId) && target.Is(CustomRoles.CovenLeader)) return false;
         
         return addon switch
         {
@@ -488,9 +491,12 @@ public static class CustomRolesHelper
     public static bool IsPlayerImpostorTeam(this PlayerState player, bool onlyMainRole = false)
     {
         if (!onlyMainRole)
+        {
+            if (player.SubRoles.Contains(CustomRoles.Madmate)) return true;
             if (player.SubRoles.Any(x => (x.IsConverted() || x is CustomRoles.Admired) && x is not CustomRoles.Madmate)) return false;
+        }
 
-        return player.MainRole.IsImpostor() || player.MainRole.GetCustomRoleType() is Custom_RoleType.Madmate;
+        return (player.MainRole.IsImpostor() || player.MainRole.GetCustomRoleType() is Custom_RoleType.Madmate) && !player.IsNecromancer;
     }
 
     public static bool IsPlayerCrewmateTeam(this PlayerControl player, bool onlyMainRole = false) => Main.PlayerStates.TryGetValue(player.PlayerId, out var state) && state.IsPlayerCrewmateTeam(onlyMainRole);
@@ -502,7 +508,7 @@ public static class CustomRolesHelper
             if (player.SubRoles.Any(x => (x.IsConverted()))) return false;
         }
 
-        return player.MainRole.IsCrewmate();
+        return player.MainRole.IsCrewmate() && !player.IsNecromancer;
     }
 
     public static bool IsPlayerNeutralTeam(this PlayerControl player, bool onlyMainRole = false) => Main.PlayerStates.TryGetValue(player.PlayerId, out var state) && state.IsPlayerNeutralTeam(onlyMainRole);
@@ -511,11 +517,12 @@ public static class CustomRolesHelper
         if (!onlyMainRole)
         {
             if (player.SubRoles.Contains(CustomRoles.Admired)) return false;
+            if (player.SubRoles.Contains(CustomRoles.Enchanted) || player.SubRoles.Contains(CustomRoles.Madmate)) return false;
             if (player.SubRoles.Any(x => (x.IsConverted() && x is not CustomRoles.Madmate or CustomRoles.Enchanted))) return true;
         }
 
         // Imp roles like crewposter and parasite is counted as netural, but should be treated as impostor team in general
-        return player.MainRole.IsNeutral() && player.MainRole.GetCustomRoleType() is not Custom_RoleType.Madmate;
+        return player.MainRole.IsNeutral() && player.MainRole.GetCustomRoleType() is not Custom_RoleType.Madmate && !player.IsNecromancer;
     }
 
     public static bool IsPlayerCovenTeam(this PlayerControl player, bool onlyMainRole = false) => Main.PlayerStates.TryGetValue(player.PlayerId, out var state) && state.IsPlayerCovenTeam(onlyMainRole);
@@ -528,7 +535,7 @@ public static class CustomRolesHelper
             if (player.SubRoles.Any(x => (x.IsConverted() && x is not CustomRoles.Enchanted))) return false;
         }
 
-        return player.MainRole.IsCoven();
+        return player.MainRole.IsCoven() || player.IsNecromancer;
     }
     public static bool CheckAddonConfilct(CustomRoles role, PlayerControl pc, bool checkLimitAddons = true, bool checkConditions = true)
     {
