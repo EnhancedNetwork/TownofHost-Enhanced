676d3082ae98096ab70cc48006f1150e32da3801
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-03-28 00:13:53 +0800

    Change back +25 patch

--- Patches/ServerVersionPatch.cs
+++ Patches/ServerVersionPatch.cs
@@ -14,7 +14,7 @@ class ServerUpdatePatch
             // Changing server version for AU mods
 
             //if (!Main.VersionCheat.Value)
-            // __result += 25;
+            __result += 25;
             Logger.Info($"IsOnlineGame: {__result}", "VersionServer");
         }
     }
