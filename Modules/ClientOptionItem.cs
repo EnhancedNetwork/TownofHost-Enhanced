d531f588dbc95d252330bd9d2025f9c148822729
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-04-11 08:37:35 +0800

    Only change x position to match 3rd row

--- Modules/ClientOptionItem.cs
+++ Modules/ClientOptionItem.cs
@@ -37,7 +37,7 @@ public class ClientOptionItem
                 CustomBackground.gameObject.SetActive(false);
 
                 var closeButton = Object.Instantiate(mouseMoveToggle, CustomBackground.transform);
-                closeButton.transform.localPosition = new(3.0f, -2.5f, -6f);
+                closeButton.transform.localPosition = new(2.6f, -2.3f, -6f);
                 closeButton.name = "Back";
                 closeButton.Text.text = Translator.GetString("Back");
                 closeButton.Background.color = Palette.DisabledGrey;
