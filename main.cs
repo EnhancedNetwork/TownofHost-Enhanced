12e2da879ad6d795ad1d718ed84747406742cf02
Limeau <148590907+Limeau@users.noreply.github.com>
2025-04-12 20:08:07 -0500

    v1.8.0 dev 1

--- main.cs
+++ main.cs
@@ -46,8 +46,8 @@ public class Main : BasePlugin
 
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
     public const string PluginVersion = "2025.0410.172.00"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "1.7.2";
-    public const string PluginDisplaySuffix = "";
+    public const string PluginDisplayVersion = "1.8.0";
+    public const string PluginDisplaySuffix = " Dev 1";
     public const string SupportedVersionAU = "2025.3.25"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
