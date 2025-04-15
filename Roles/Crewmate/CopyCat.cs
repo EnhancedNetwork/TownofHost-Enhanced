035d54e041bd47a6b166cb2a68976da37b9d3090
Marg <51059123+MargaretTheFool@users.noreply.github.com>
2025-04-15 14:35:07 -0400

    hopefully fix people being able to see copycat and necromancer when not dead

--- Roles/Crewmate/CopyCat.cs
+++ Roles/Crewmate/CopyCat.cs
@@ -202,7 +202,7 @@ internal class CopyCat : RoleBase
     }
     public static string CopycatReminder(PlayerControl seer, PlayerControl seen = null, bool isForMeeting = false, bool isForHud = false)
     {
-        if (playerIdList.Contains(seen.PlayerId) && !seen.Is(CustomRoles.CopyCat) && ((!seer.IsAlive() && Altruist.HasEnabled ? !seer.IsMurderedThisRound() : true) || seer == seen) && seen.IsAlive())
+        if (playerIdList.Contains(seen.PlayerId) && !seen.Is(CustomRoles.CopyCat) && !seer.IsAlive() && seen.IsAlive())
         {
             return $"<size=1.5><i>{CustomRoles.CopyCat.ToColoredString()}</i></size>";
         }
