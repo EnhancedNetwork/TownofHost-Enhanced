6c53a1c8a13115283ea41e9834b4ea3bb5d37ce0
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-05-24 04:20:15 +0800

    230 Beta 1

--- main.cs
+++ main.cs
@@ -53,16 +53,16 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2025.0509.230.00090"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.3.0 Alpha 9";
+    public const string PluginVersion = "2025.0525.230.00100"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.3.0 Beta 1";
     public static readonly List<(int year, int month, int day, int revision)> SupportedVersionAU =
         [
-            (2024, 8, 11, 0) // 2025.3.25 & 16.0.0
+            (2025, 4, 15, 0) // 2025.3.25 & 16.0.0
         ];
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool devRelease = true; // Latest: V2.3.0 Alpha 9
-    public static readonly bool canaryRelease = false; // Latest: V2.2.0 Beta 4
+    public static readonly bool devRelease = false; // Latest: V2.3.0 Alpha 9
+    public static readonly bool canaryRelease = true; // Latest: V2.2.0 Beta 4
     public static readonly bool fullRelease = false; // Latest: V2.2.0
 
     public static bool hasAccess = true;
