b54a77c6f4680fcfa1fd7f49f3c7864d4369198d
Marg <51059123+MargaretTheFool@users.noreply.github.com>
2025-06-20 14:17:37 -0400

    change version

--- main.cs
+++ main.cs
@@ -53,8 +53,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2025.0615.230.00400"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.3.0 Beta 4";
+    public const string PluginVersion = "2025.0620.230.00500"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.3.0 Beta 5";
     public static readonly List<(int year, int month, int day, int revision)> SupportedVersionAU =
         [
             (2025, 4, 15, 0) // 2025.3.25 & 16.0.0
