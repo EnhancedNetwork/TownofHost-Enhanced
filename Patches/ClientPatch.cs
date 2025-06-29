21a3c5845e65e47c911f751f9c182d3db8bd7157
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-06-29 17:25:39 +0800

    Cancel isminor patch

--- Patches/ClientPatch.cs
+++ Patches/ClientPatch.cs
@@ -170,12 +170,14 @@ internal class IsMinorByDatePatch
 { 
     public static bool Prefix(EOSManager __instance, ref bool __result)
     {
+        /*
         if (DebugModeManager.AmDebugger)
         {
             Logger.Info("Skipped Eos is minor check", "IsMinorByDatePatch");
             __result = false;
             return false;
         }
+        */
 
         return true;
     }
