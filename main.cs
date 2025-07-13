3bf2155c20bd20ef993b6cd09d39663aea27184b
Polkasworn <lime19844@gmail.com>
2025-07-13 13:15:43 +0100

    Revert "Update main.cs"
    This reverts commit f969e46803ffd79548067aba0b4404d03552d4ff.

--- main.cs
+++ main.cs
@@ -45,10 +45,10 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
-    public const string PluginVersion = "2025.0619.200.99"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginVersion = "2025.0501.180.00"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "2.0.0";
     public const string PluginDisplaySuffix = "Pre-Alpha";
-    public const string SupportedVersionAU = "2025.4.15"; // Changed becasue Dark theme works at this version.
+    public const string SupportedVersionAU = "2025.6.19"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
     public static readonly bool devRelease = false; // Latest: v2.2.0 Alpha 17.1
