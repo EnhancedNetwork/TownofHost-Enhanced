46d2bc438c4f1c24b8b01ace2cdc39e9f0bd2c35
Polkasworn <148590907+Polkasworn@users.noreply.github.com>
2025-07-28 21:45:17 +0100

    port fix steam bool options from ehr

--- Patches/GameOptionsMenuPatch.cs
+++ Patches/GameOptionsMenuPatch.cs
@@ -464,6 +464,24 @@ public static class ToggleOptionPatch
         }
         return true;
     }
+    
+    // Thanks Gurge44 - https://github.com/Gurge44/EndlessHostRoles
+    [HarmonyPatch(nameof(ToggleOption.Toggle))]
+    [HarmonyPrefix]
+    private static bool TogglePrefix(ToggleOption __instance)
+    {
+        if (ModGameOptionsMenu.OptionList.TryGetValue(__instance, out int index))
+        {
+            __instance.CheckMark.enabled = !__instance.CheckMark.enabled;
+            OptionItem item = OptionItem.AllOptions[index];
+            item.SetValue(__instance.GetBool() ? 1 : 0);
+            __instance.OnValueChanged.Invoke(__instance);
+            NotificationPopperPatch.AddSettingsChangeMessage(index, item, true);
+            return false;
+        }
+
+        return true;
+    }
 }
 [HarmonyPatch(typeof(NumberOption))]
 public static class NumberOptionPatch
