ffa521f6d39ee918246bc99b098adccc596ac238
Polkasworn <148590907+Polkasworn@users.noreply.github.com>
2025-07-28 21:45:48 +0100

    test 0.1

--- main.cs
+++ main.cs
@@ -47,7 +47,7 @@ public class Main : BasePlugin
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
     public const string PluginVersion = "2025.0727.200.01"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "2.0.0";
-    public const string PluginDisplaySuffix = " Test 0";
+    public const string PluginDisplaySuffix = " Test 0.1";
     public const string SupportedVersionAU = "2025.4.15"; // Changed becasue Dark theme works at this version.
 
     /******************* Change one of the three variables to true before making a release. *******************/
