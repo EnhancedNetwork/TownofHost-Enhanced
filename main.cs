69d26e10f59c2e57722260e6ca9043b60cc14018
BatmenzDW <turtleterracotta@gmail.com>
2026-02-04 22:31:42 -0500

    Beta 2

--- main.cs
+++ main.cs
@@ -55,8 +55,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "2026.0127.242.00100"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.4.2 Beta 1";
+    public const string PluginVersion = "2026.0127.242.00200"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.4.2 Beta 2";
     public static readonly List<(int year, int month, int day, int revision)> SupportedVersionAU =
     [
         (2025, 11, 18, 0) // 2025.11.18 & 17.1
@@ -64,7 +64,7 @@ public class Main : BasePlugin
 
     /******************* Change one of the three variables to true before making a release. *******************/
     public static readonly bool devRelease = false; // Latest: v3.0.0 Alpha 13
-    public static readonly bool canaryRelease = true; // Latest: V2.4.2 Beta 1
+    public static readonly bool canaryRelease = true; // Latest: V2.4.2 Beta 2
     public static readonly bool fullRelease = false; // Latest: V2.4.1
 
     public static bool hasAccess = true;
