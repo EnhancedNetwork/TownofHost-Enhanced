e89414dbeeaae92f5b5465bb8ffc7c9d67672a9c
BatmenzDW <turtleterracotta@gmail.com>
2026-02-22 17:18:41 -0500

    logging

--- main.cs
+++ main.cs
@@ -57,8 +57,8 @@ public class Main : BasePlugin
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
     public const string PluginGuid4 = "90759289-1d0d-494b-b36c-839f93ae0df1"; // for matchmaking token
-    public const string PluginVersion = "2026.0220.242.07000"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.4.2 Beta 7";
+    public const string PluginVersion = "2026.0220.242.07100"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.4.2 Beta 7.1";
     public static readonly List<(int year, int month, int day, int revision)> SupportedVersionAU =
     [
         (2025, 11, 18, 0), // 2025.11.18 & 17.1
@@ -586,7 +586,7 @@ public class Main : BasePlugin
         TOHE.Logger.Disable("RpcSetNamePrivate");
         // TOHE.Logger.Disable("SendRPC");
         TOHE.Logger.Disable("KnowRoleTarget");
-        TOHE.Logger.Disable("CustomRpcSender");
+        // TOHE.Logger.Disable("CustomRpcSender");
         if (!DebugModeManager.AmDebugger)
         {
             TOHE.Logger.Disable("2018k");
@@ -606,7 +606,7 @@ public class Main : BasePlugin
             TOHE.Logger.Disable("SyncCustomSettings");
             TOHE.Logger.Disable("NR");
             TOHE.Logger.Disable("RpcSetName");
-            // TOHE.Logger.Disable("CustomRpcSender");
+            TOHE.Logger.Disable("CustomRpcSender");
             // TOHE.Logger.Disable("KnowRoleTarget");
         }
         //TOHE.Logger.isDetail = true;
